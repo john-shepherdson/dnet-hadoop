a52d5bde4f1336769260bea06602b5c954933c55
Claudio Atzori <claudio.atzori@isti.cnr.it>
2019-11-06 17:45:52 +0100

    simplified import procedure, maps the infospace as hive tables

--- dhp-workflows/dhp-graph-mapper/src/main/java/eu/dnetlib/dhp/graph/SparkGraphImporterJob.java
+++ dhp-workflows/dhp-graph-mapper/src/main/java/eu/dnetlib/dhp/graph/SparkGraphImporterJob.java
@@ -3,11 +3,11 @@ package eu.dnetlib.dhp.graph;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import eu.dnetlib.dhp.application.ArgumentApplicationParser;
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.io.Text;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.SparkSession;
 import scala.Tuple2;
 
@@ -19,29 +19,27 @@ public class SparkGraphImporterJob {
         parser.parseArgument(args);
         final SparkSession spark = SparkSession
                 .builder()
-                .appName("ImportGraph")
+                .appName(SparkGraphImporterJob.class.getSimpleName())
                 .master(parser.get("master"))
+                .config("hive.metastore.uris", parser.get("hive.metastore.uris"))
+                .enableHiveSupport()
                 .getOrCreate();
+
         final JavaSparkContext sc = new JavaSparkContext(spark.sparkContext());
         final String inputPath = parser.get("sourcePath");
-        final String outputPath = parser.get("targetPath");
-
-        final String filter = parser.get("filter");
 
         // Read the input file and convert it into RDD of serializable object
-        final JavaRDD<Tuple2<String, String>> inputRDD = sc.sequenceFile(inputPath, Text.class, Text.class)
-                .map(item -> new Tuple2<>(item._1.toString(), item._2.toString()));
-
         GraphMappingUtils.types.forEach((name, clazz) -> {
-            if (StringUtils.isNotBlank(filter) || filter.toLowerCase().contains(name)) {
-                spark.createDataset(inputRDD
-                        .filter(s -> s._1().equals(clazz.getName()))
-                        .map(Tuple2::_2)
-                        .map(s -> new ObjectMapper().readValue(s, clazz))
-                        .rdd(), Encoders.bean(clazz))
-                        .write()
-                        .save(outputPath + "/" + name);
-            }
+            final JavaRDD<Tuple2<String, String>> inputRDD = sc.sequenceFile(inputPath + "/" + name, Text.class, Text.class)
+                    .map(item -> new Tuple2<>(item._1.toString(), item._2.toString()));
+            spark.createDataset(inputRDD
+                    .filter(s -> s._1().equals(clazz.getName()))
+                    .map(Tuple2::_2)
+                    .map(s -> new ObjectMapper().readValue(s, clazz))
+                    .rdd(), Encoders.bean(clazz))
+                    .write()
+                    .mode(SaveMode.Overwrite)
+                    .saveAsTable("openaire." + name);
         });
 
     }
