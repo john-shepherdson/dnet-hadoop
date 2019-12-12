545e940007ac2003d61d06dc018419ff153b7d01
miconis <m.debonis991@gmail.com>
2019-12-12 15:36:41 +0100

    implementation of the mergeFrom for the Datasources

--- dhp-schemas/src/main/java/eu/dnetlib/dhp/schema/oaf/Datasource.java
+++ dhp-schemas/src/main/java/eu/dnetlib/dhp/schema/oaf/Datasource.java
@@ -78,7 +78,7 @@ public class Datasource extends OafEntity implements Serializable {
 
     private Field<String> certificates;
 
-    private List< KeyValue> policies;
+    private List<KeyValue> policies;
 
     private Journal journal;
 
@@ -360,5 +360,68 @@ public class Datasource extends OafEntity implements Serializable {
 
     public void setJournal(Journal journal) {
         this.journal = journal;
+    }
+
+    @Override
+    public void mergeFrom(OafEntity e) {
+        super.mergeFrom(e);
+
+        Datasource d = (Datasource)e;
+
+        datasourcetype = d.getDatasourcetype() != null && compareTrust(this, e)<0? d.getDatasourcetype() : datasourcetype;
+        openairecompatibility = d.getOpenairecompatibility() != null && compareTrust(this, e)<0? d.getOpenairecompatibility() : openairecompatibility;
+        officialname = d.getOfficialname() != null && compareTrust(this, e)<0? d.getOfficialname() : officialname;
+        englishname = d.getEnglishname() != null && compareTrust(this, e)<0? d.getEnglishname() : officialname;
+        websiteurl = d.getWebsiteurl() != null && compareTrust(this, e)<0? d.getWebsiteurl() : websiteurl;
+        logourl = d.getLogourl() != null && compareTrust(this, e)<0? d.getLogourl() : getLogourl();
+        contactemail = d.getContactemail() != null && compareTrust(this, e)<0? d.getContactemail() : contactemail;
+        namespaceprefix = d.getNamespaceprefix() != null && compareTrust(this, e)<0? d.getNamespaceprefix() : namespaceprefix;
+        latitude = d.getLatitude() != null && compareTrust(this, e)<0? d.getLatitude() : latitude;
+        longitude = d.getLongitude() != null && compareTrust(this, e)<0? d.getLongitude() : longitude;
+        dateofvalidation = d.getDateofvalidation() != null && compareTrust(this, e)<0? d.getDateofvalidation() : dateofvalidation;
+        description = d.getDescription() != null && compareTrust(this, e)<0? d.getDescription() : description;
+        subjects = mergeLists(subjects, d.getSubjects());
+
+        // opendoar specific fields (od*)
+        odnumberofitems = d.getOdnumberofitems() != null && compareTrust(this, e)<0? d.getOdnumberofitems() : odnumberofitems;
+        odnumberofitemsdate = d.getOdnumberofitemsdate() != null && compareTrust(this, e)<0? d.getOdnumberofitemsdate() : odnumberofitemsdate;
+        odpolicies = d.getOdpolicies() != null && compareTrust(this, e)<0? d.getOdpolicies() : odpolicies;
+        odlanguages = mergeLists(odlanguages, d.getOdlanguages());
+        odcontenttypes = mergeLists(odcontenttypes, d.getOdcontenttypes());
+        accessinfopackage = mergeLists(accessinfopackage, d.getAccessinfopackage());
+
+        // re3data fields
+        releasestartdate = d.getReleasestartdate() != null && compareTrust(this, e)<0? d.getReleasestartdate() : releasestartdate;
+        releaseenddate = d.getReleaseenddate() != null && compareTrust(this, e)<0? d.getReleaseenddate() : releaseenddate;
+        missionstatementurl = d.getMissionstatementurl() != null && compareTrust(this, e)<0? d.getMissionstatementurl() : missionstatementurl;
+        dataprovider = d.getDataprovider() != null && compareTrust(this, e)<0? d.getDataprovider() : dataprovider;
+        serviceprovider = d.getServiceprovider() != null && compareTrust(this, e)<0? d.getServiceprovider() : serviceprovider;
+
+        // {open, restricted or closed}
+        databaseaccesstype = d.getDatabaseaccesstype() != null && compareTrust(this, e)<0? d.getDatabaseaccesstype() : databaseaccesstype;
+
+        // {open, restricted or closed}
+        datauploadtype = d.getDatauploadtype() != null && compareTrust(this, e)<0? d.getDatauploadtype() : datauploadtype;
+
+        // {feeRequired, registration, other}
+        databaseaccessrestriction = d.getDatabaseaccessrestriction() != null && compareTrust(this, e)<0? d.getDatabaseaccessrestriction() : databaseaccessrestriction;
+
+        // {feeRequired, registration, other}
+        datauploadrestriction = d.getDatauploadrestriction() != null && compareTrust(this, e)<0? d.getDatauploadrestriction() : datauploadrestriction;
+
+        versioning = d.getVersioning() != null && compareTrust(this, e)<0? d.getVersioning() : versioning;
+        citationguidelineurl = d.getCitationguidelineurl() != null && compareTrust(this, e)<0? d.getCitationguidelineurl() : citationguidelineurl;
+
+        //{yes, no, unknown}
+        qualitymanagementkind = d.getQualitymanagementkind() != null && compareTrust(this, e)<0? d.getQualitymanagementkind() : qualitymanagementkind;
+        pidsystems = d.getPidsystems() != null && compareTrust(this, e)<0? d.getPidsystems() : pidsystems;
+
+        certificates = d.getCertificates() != null && compareTrust(this, e)<0? d.getCertificates() : certificates;
+
+        policies = mergeLists(policies, d.getPolicies());
+
+        journal = d.getJournal() != null && compareTrust(this, e)<0? d.getJournal() : journal;
+
+        mergeOAFDataInfo(e);
     }
 }
