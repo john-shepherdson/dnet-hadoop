8b0ba3d76aeb176c31752047b475826824fcbce1
Claudio Atzori <claudio.atzori@isti.cnr.it>
2020-03-23 17:40:39 +0100

    posprocessing script correctly run as hive2 action

--- dhp-workflows/dhp-graph-mapper/src/main/resources/eu/dnetlib/dhp/graph/oozie_app/lib/scripts/postprocessing.sql
+++ dhp-workflows/dhp-graph-mapper/src/main/resources/eu/dnetlib/dhp/graph/oozie_app/lib/scripts/postprocessing.sql
@@ -1,4 +1,6 @@
-CREATE view result as
+DROP VIEW IF EXISTS ${hive_db_name}.result;
+
+CREATE VIEW IF NOT EXISTS result as
     select id, dateofcollection, bestaccessright, datainfo, collectedfrom, pid, author, resulttype, language, country, subject, description, dateofacceptance, embargoenddate, resourcetype, context, instance from ${hive_db_name}.publication p
     union all
     select id, dateofcollection, bestaccessright, datainfo, collectedfrom, pid, author, resulttype, language, country, subject, description, dateofacceptance, embargoenddate, resourcetype, context, instance from ${hive_db_name}.dataset d
