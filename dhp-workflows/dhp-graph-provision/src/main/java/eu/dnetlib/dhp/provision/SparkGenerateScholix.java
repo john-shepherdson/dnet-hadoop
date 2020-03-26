a768226e520c3fb70149395481c15f512a66e7d6
Sandro La Bruzzo <sandro.labruzzo@isti.cnr.it>
2020-03-26 09:40:50 +0100

    updated generate scholix to generate json

--- dhp-workflows/dhp-graph-provision/src/main/java/eu/dnetlib/dhp/provision/SparkGenerateScholix.java
+++ dhp-workflows/dhp-graph-provision/src/main/java/eu/dnetlib/dhp/provision/SparkGenerateScholix.java
@@ -1,27 +1,20 @@
 package eu.dnetlib.dhp.provision;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import eu.dnetlib.dhp.application.ArgumentApplicationParser;
 import eu.dnetlib.dhp.provision.scholix.*;
-import eu.dnetlib.dhp.provision.scholix.summary.*;
+import eu.dnetlib.dhp.provision.scholix.summary.ScholixSummary;
 import eu.dnetlib.dhp.schema.oaf.Relation;
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.StringUtils;
+import org.apache.hadoop.io.compress.GzipCodec;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.MapFunction;
-import org.apache.spark.api.java.function.PairFlatMapFunction;
-import org.apache.spark.sql.*;
-
-import  static org.apache.spark.sql.functions.col;
-
-import scala.Int;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.SaveMode;
+import org.apache.spark.sql.SparkSession;
 import scala.Tuple2;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 
 public class SparkGenerateScholix {
 
@@ -29,33 +22,25 @@ public class SparkGenerateScholix {
     public static void main(String[] args) throws Exception {
         final ArgumentApplicationParser parser = new ArgumentApplicationParser(IOUtils.toString(SparkGenerateScholix.class.getResourceAsStream("/eu/dnetlib/dhp/provision/input_generate_summary_parameters.json")));
         parser.parseArgument(args);
-
-
         SparkConf conf = new SparkConf();
         conf.set("spark.sql.shuffle.partitions","4000");
-//        conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
-//        conf.registerKryoClasses(new Class[]{
-//                ScholixSummary.class,
-//                CollectedFromType.class,
-//                SchemeValue.class,
-//                TypedIdentifier.class,
-//                Typology.class,
-//                Relation.class,
-//                Scholix.class,
-//                ScholixCollectedFrom.class,
-//                ScholixEntityId.class,
-//                ScholixIdentifier.class,
-//                ScholixRelationship.class,
-//                ScholixResource.class
-//        });
-
-
+        conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
         final SparkSession spark = SparkSession
                 .builder()
                 .config(conf)
                 .appName(SparkExtractRelationCount.class.getSimpleName())
                 .master(parser.get("master"))
                 .getOrCreate();
+
+        conf.registerKryoClasses(new Class[]{
+                Scholix.class,
+                ScholixCollectedFrom.class,
+                ScholixEntityId.class,
+                ScholixIdentifier.class,
+                ScholixRelationship.class,
+                ScholixResource.class
+        });
+
 
 
         final String graphPath = parser.get("graphPath");
@@ -71,11 +56,15 @@ public class SparkGenerateScholix {
                 .map((MapFunction<Tuple2<ScholixSummary, Relation>, Scholix>) f -> Scholix.generateScholixWithSource(f._1(), f._2()), Encoders.bean(Scholix.class));
 
         firstJoin.write().mode(SaveMode.Overwrite).save(workingDirPath+"/scholix_1");
-        firstJoin = spark.read().load(workingDirPath+"/scholix_1").as(Encoders.bean(Scholix.class));
-
-
 
         Dataset<Scholix> scholix_final = spark.read().load(workingDirPath+"/scholix_1").as(Encoders.bean(Scholix.class));
+
+        scholixSummary
+                .map((MapFunction<ScholixSummary, ScholixResource>) ScholixResource::fromSummary, Encoders.bean(ScholixResource.class))
+                .repartition(1000)
+                .write()
+                .mode(SaveMode.Overwrite)
+                .save(workingDirPath+"/scholix_target");
 
         Dataset<ScholixResource> target = spark.read().load(workingDirPath+"/scholix_target").as(Encoders.bean(ScholixResource.class));
 
@@ -87,6 +76,9 @@ public class SparkGenerateScholix {
                     scholix.generateIdentifier();
                     scholix.generatelinkPublisher();
                     return scholix;
-                }, Encoders.bean(Scholix.class)).repartition(5000).write().mode(SaveMode.Overwrite).save(workingDirPath+"/scholix_index");
+                }, Encoders.kryo(Scholix.class)).javaRDD().map(s-> {
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.writeValueAsString(s);
+        }).saveAsTextFile(workingDirPath+"/scholix_json", GzipCodec.class);
     }
 }
