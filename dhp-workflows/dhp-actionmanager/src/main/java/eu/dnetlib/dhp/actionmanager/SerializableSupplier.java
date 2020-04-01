5b459bcc4750fb9a1569a6475821607ed26c3f75
pjacewicz <przemyslaw.jacewicz@icm.edu.pl>
2020-04-01 18:44:35 +0200

    [dhp-actionmanager] promoting spark job added

--- dhp-workflows/dhp-actionmanager/src/main/java/eu/dnetlib/dhp/actionmanager/SerializableSupplier.java
+++
@@ -1,8 +0,0 @@
-package eu.dnetlib.dhp.actionmanager;
-
-import java.io.Serializable;
-import java.util.function.Supplier;
-
-@FunctionalInterface
-public interface SerializableSupplier<T> extends Supplier<T>, Serializable {
-}
