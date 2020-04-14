e0038bde5baf1a3903b9065fa28cad8fad1e41ac
miriam.baglioni <miriam.baglioni@isti.cnr.it>
2020-04-14 15:32:12 +0200

    Support class to serialize/deserialize the association project, set of linked results

--- dhp-workflows/dhp-propagation/src/main/java/eu/dnetlib/dhp/projecttoresult/ProjectResultSet.java
+++ dhp-workflows/dhp-propagation/src/main/java/eu/dnetlib/dhp/projecttoresult/ProjectResultSet.java
@@ -1,4 +1,25 @@
 package eu.dnetlib.dhp.projecttoresult;
 
-public class ProjectResultSet {
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class ProjectResultSet implements Serializable {
+    private String projectId;
+    private ArrayList<String> resultSet;
+
+    public String getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(String projectId) {
+        this.projectId = projectId;
+    }
+
+    public ArrayList<String> getResultSet() {
+        return resultSet;
+    }
+
+    public void setResultSet(ArrayList<String> resultSet) {
+        this.resultSet = resultSet;
+    }
 }
