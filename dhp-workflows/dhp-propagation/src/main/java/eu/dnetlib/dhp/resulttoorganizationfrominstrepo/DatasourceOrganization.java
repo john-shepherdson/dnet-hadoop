5a3487280df7520a21c6e6f888c6e302c7fbfea1
miriam.baglioni <miriam.baglioni@isti.cnr.it>
2020-04-15 12:24:36 +0200

    classes to serialize/deserialize the prepared data

--- dhp-workflows/dhp-propagation/src/main/java/eu/dnetlib/dhp/resulttoorganizationfrominstrepo/DatasourceOrganization.java
+++ dhp-workflows/dhp-propagation/src/main/java/eu/dnetlib/dhp/resulttoorganizationfrominstrepo/DatasourceOrganization.java
@@ -1,4 +1,25 @@
 package eu.dnetlib.dhp.resulttoorganizationfrominstrepo;
 
-public class DatasourceOrganization {
+import java.io.Serializable;
+
+public class DatasourceOrganization implements Serializable {
+
+    private String datasourceId;
+    private String organizationId;
+
+    public String getDatasourceId() {
+        return datasourceId;
+    }
+
+    public void setDatasourceId(String datasourceId) {
+        this.datasourceId = datasourceId;
+    }
+
+    public String getOrganizationId() {
+        return organizationId;
+    }
+
+    public void setOrganizationId(String organizationId) {
+        this.organizationId = organizationId;
+    }
 }
--- dhp-workflows/dhp-propagation/src/main/java/eu/dnetlib/dhp/resulttoorganizationfrominstrepo/ResultOrganizationSet.java
+++ dhp-workflows/dhp-propagation/src/main/java/eu/dnetlib/dhp/resulttoorganizationfrominstrepo/ResultOrganizationSet.java
@@ -1,4 +1,25 @@
 package eu.dnetlib.dhp.resulttoorganizationfrominstrepo;
 
-public class ResultOrganizationSet {
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class ResultOrganizationSet implements Serializable {
+    private String resultId;
+    private ArrayList<String> organizationSet;
+
+    public String getResultId() {
+        return resultId;
+    }
+
+    public void setResultId(String resultId) {
+        this.resultId = resultId;
+    }
+
+    public ArrayList<String> getOrganizationSet() {
+        return organizationSet;
+    }
+
+    public void setOrganizationSet(ArrayList<String> organizationSet) {
+        this.organizationSet = organizationSet;
+    }
 }
