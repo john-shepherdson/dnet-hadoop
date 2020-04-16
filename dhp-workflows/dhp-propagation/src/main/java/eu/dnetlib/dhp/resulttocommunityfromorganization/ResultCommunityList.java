fff1e5ec393b210e7c8987a81de6b74a0a7d0f0d
miriam.baglioni <miriam.baglioni@isti.cnr.it>
2020-04-16 18:44:43 +0200

    classes to (de)serialize the data provided in the preparation step

--- dhp-workflows/dhp-propagation/src/main/java/eu/dnetlib/dhp/resulttocommunityfromorganization/ResultCommunityList.java
+++ dhp-workflows/dhp-propagation/src/main/java/eu/dnetlib/dhp/resulttocommunityfromorganization/ResultCommunityList.java
@@ -1,4 +1,25 @@
 package eu.dnetlib.dhp.resulttocommunityfromorganization;
 
-public class ResultCommunityList {
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class ResultCommunityList implements Serializable {
+    private String resultId;
+    private ArrayList<String> communityList;
+
+    public String getResultId() {
+        return resultId;
+    }
+
+    public void setResultId(String resultId) {
+        this.resultId = resultId;
+    }
+
+    public ArrayList<String> getCommunityList() {
+        return communityList;
+    }
+
+    public void setCommunityList(ArrayList<String> communityList) {
+        this.communityList = communityList;
+    }
 }
--- dhp-workflows/dhp-propagation/src/main/java/eu/dnetlib/dhp/resulttocommunityfromorganization/ResultOrganizations.java
+++ dhp-workflows/dhp-propagation/src/main/java/eu/dnetlib/dhp/resulttocommunityfromorganization/ResultOrganizations.java
@@ -1,4 +1,34 @@
 package eu.dnetlib.dhp.resulttocommunityfromorganization;
 
-public class ResultOrganizations {
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class ResultOrganizations implements Serializable {
+    private String resultId;
+    private String orgId;
+    private ArrayList<String> merges;
+
+    public String getResultId() {
+        return resultId;
+    }
+
+    public void setResultId(String resultId) {
+        this.resultId = resultId;
+    }
+
+    public String getOrgId() {
+        return orgId;
+    }
+
+    public void setOrgId(String orgId) {
+        this.orgId = orgId;
+    }
+
+    public ArrayList<String> getMerges() {
+        return merges;
+    }
+
+    public void setMerges(ArrayList<String> merges) {
+        this.merges = merges;
+    }
 }
