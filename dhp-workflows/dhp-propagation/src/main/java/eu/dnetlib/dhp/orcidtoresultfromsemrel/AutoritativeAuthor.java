ac3ad25b364b002d0fe20a5e269b519bf66abc18
miriam.baglioni <miriam.baglioni@isti.cnr.it>
2020-04-16 15:56:33 +0200

    to (de)serialize needed information of the author to determine if the orcid can be passed (name, surname, fullname (?), orcid)

--- dhp-workflows/dhp-propagation/src/main/java/eu/dnetlib/dhp/orcidtoresultfromsemrel/AutoritativeAuthor.java
+++ dhp-workflows/dhp-propagation/src/main/java/eu/dnetlib/dhp/orcidtoresultfromsemrel/AutoritativeAuthor.java
@@ -1,4 +1,40 @@
 package eu.dnetlib.dhp.orcidtoresultfromsemrel;
 
 public class AutoritativeAuthor {
+    String name;
+    String surname;
+    String fullname;
+    String orcid;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getOrcid() {
+        return orcid;
+    }
+
+    public void setOrcid(String orcid) {
+        this.orcid = orcid;
+    }
 }
