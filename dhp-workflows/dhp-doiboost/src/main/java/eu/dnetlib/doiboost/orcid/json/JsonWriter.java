4a6aea1a3707bb34aa5a2105e5edee7e2aca862a
Enrico Ottonello <enrico.ottonello@isti.cnr.it>
2020-04-23 15:25:39 +0200

    fix formtat problem

--- dhp-workflows/dhp-doiboost/src/main/java/eu/dnetlib/doiboost/orcid/json/JsonWriter.java
+++ dhp-workflows/dhp-doiboost/src/main/java/eu/dnetlib/doiboost/orcid/json/JsonWriter.java
@@ -1,20 +1,18 @@
 package eu.dnetlib.doiboost.orcid.json;
 
 import com.google.gson.JsonObject;
-
 import eu.dnetlib.doiboost.orcid.model.AuthorData;
-
 
 public class JsonWriter {
 
-	public static String create(AuthorData authorData) {
-		JsonObject author = new JsonObject();
-		author.addProperty("oid", authorData.getOid());
-		author.addProperty("name", authorData.getName());
-		author.addProperty("surname", authorData.getSurname());
-		if (authorData.getCreditName()!=null) {
-			author.addProperty("creditname", authorData.getCreditName());
-		}
-		return author.toString();
-	}
+    public static String create(AuthorData authorData) {
+        JsonObject author = new JsonObject();
+        author.addProperty("oid", authorData.getOid());
+        author.addProperty("name", authorData.getName());
+        author.addProperty("surname", authorData.getSurname());
+        if (authorData.getCreditName() != null) {
+            author.addProperty("creditname", authorData.getCreditName());
+        }
+        return author.toString();
+    }
 }
