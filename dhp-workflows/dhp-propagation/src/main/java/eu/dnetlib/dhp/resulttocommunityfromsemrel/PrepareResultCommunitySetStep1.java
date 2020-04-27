e000754c92cb679518bb086b189b2306559881d0
miriam.baglioni <miriam.baglioni@isti.cnr.it>
2020-04-27 10:34:03 +0200

    refactoring

--- dhp-workflows/dhp-propagation/src/main/java/eu/dnetlib/dhp/resulttocommunityfromsemrel/PrepareResultCommunitySetStep1.java
+++ dhp-workflows/dhp-propagation/src/main/java/eu/dnetlib/dhp/resulttocommunityfromsemrel/PrepareResultCommunitySetStep1.java
@@ -1,5 +1,8 @@
 package eu.dnetlib.dhp.resulttocommunityfromsemrel;
 
+import static eu.dnetlib.dhp.PropagationConstant.*;
+import static eu.dnetlib.dhp.common.SparkSessionSupport.runWithSparkHiveSession;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import eu.dnetlib.dhp.QueryInformationSystem;
@@ -7,6 +10,8 @@ import eu.dnetlib.dhp.application.ArgumentApplicationParser;
 import eu.dnetlib.dhp.resulttocommunityfromorganization.ResultCommunityList;
 import eu.dnetlib.dhp.schema.oaf.Relation;
 import eu.dnetlib.dhp.schema.oaf.Result;
+import java.util.Arrays;
+import java.util.List;
 import org.apache.commons.io.IOUtils;
 import org.apache.hadoop.io.compress.GzipCodec;
 import org.apache.spark.SparkConf;
@@ -18,23 +23,18 @@ import org.apache.spark.sql.SparkSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
-import java.util.List;
-
-import static eu.dnetlib.dhp.PropagationConstant.*;
-import static eu.dnetlib.dhp.common.SparkSessionSupport.runWithSparkHiveSession;
-
 public class PrepareResultCommunitySetStep1 {
     private static final Logger log = LoggerFactory.getLogger(PrepareResultCommunitySetStep1.class);
 
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     public static void main(String[] args) throws Exception {
-        String jsonConfiguration = IOUtils.toString(PrepareResultCommunitySetStep1.class
-                .getResourceAsStream("/eu/dnetlib/dhp/resulttocommunityfromsemrel/input_preparecommunitytoresult_parameters.json"));
+        String jsonConfiguration =
+                IOUtils.toString(
+                        PrepareResultCommunitySetStep1.class.getResourceAsStream(
+                                "/eu/dnetlib/dhp/resulttocommunityfromsemrel/input_preparecommunitytoresult_parameters.json"));
 
-        final ArgumentApplicationParser parser = new ArgumentApplicationParser(
-                jsonConfiguration);
+        final ArgumentApplicationParser parser = new ArgumentApplicationParser(jsonConfiguration);
 
         parser.parseArgument(args);
 
@@ -62,30 +62,47 @@ public class PrepareResultCommunitySetStep1 {
         final List<String> communityIdList = QueryInformationSystem.getCommunityList(isLookupUrl);
         log.info("communityIdList: {}", new Gson().toJson(communityIdList));
 
-        final String resultType = resultClassName.substring(resultClassName.lastIndexOf(".") + 1).toLowerCase();
+        final String resultType =
+                resultClassName.substring(resultClassName.lastIndexOf(".") + 1).toLowerCase();
         log.info("resultType: {}", resultType);
 
+        Class<? extends Result> resultClazz =
+                (Class<? extends Result>) Class.forName(resultClassName);
 
-        Class<? extends Result> resultClazz = (Class<? extends Result>) Class.forName(resultClassName);
-
-
-        runWithSparkHiveSession(conf, isSparkSessionManaged,
+        runWithSparkHiveSession(
+                conf,
+                isSparkSessionManaged,
                 spark -> {
                     if (isTest(parser)) {
                         removeOutputDir(spark, outputPath);
                     }
-                    prepareInfo(spark,  inputPath, outputPath, allowedsemrel, resultClazz, resultType,
+                    prepareInfo(
+                            spark,
+                            inputPath,
+                            outputPath,
+                            allowedsemrel,
+                            resultClazz,
+                            resultType,
                             communityIdList);
                 });
     }
 
-    private static <R extends Result> void prepareInfo(SparkSession spark, String inputPath, String outputPath,
-                                                       List<String> allowedsemrel, Class<R> resultClazz, String resultType,
-                                                       List<String> communityIdList) {
-        //read the relation table and the table related to the result it is using
+    private static <R extends Result> void prepareInfo(
+            SparkSession spark,
+            String inputPath,
+            String outputPath,
+            List<String> allowedsemrel,
+            Class<R> resultClazz,
+            String resultType,
+            List<String> communityIdList) {
+        // read the relation table and the table related to the result it is using
         final JavaSparkContext sc = new JavaSparkContext(spark.sparkContext());
-        org.apache.spark.sql.Dataset<Relation> relation = spark.createDataset(sc.textFile(inputPath + "/relation")
-                .map(item -> OBJECT_MAPPER.readValue(item, Relation.class)).rdd(), Encoders.bean(Relation.class));
+        org.apache.spark.sql.Dataset<Relation> relation =
+                spark.createDataset(
+                        sc.textFile(inputPath + "/relation")
+                                .map(item -> OBJECT_MAPPER.readValue(item, Relation.class))
+                                .rdd(),
+                        Encoders.bean(Relation.class));
         relation.createOrReplaceTempView("relation");
 
         log.info("Reading Graph table from: {}", inputPath + "/" + resultType);
@@ -93,16 +110,18 @@ public class PrepareResultCommunitySetStep1 {
 
         result.createOrReplaceTempView("result");
 
-        getPossibleResultcommunityAssociation(spark, allowedsemrel, outputPath + "/" + resultType, communityIdList);
-
+        getPossibleResultcommunityAssociation(
+                spark, allowedsemrel, outputPath + "/" + resultType, communityIdList);
     }
 
-    private static void getPossibleResultcommunityAssociation(SparkSession spark, List<String> allowedsemrel, String outputPath,
-                                                              List<String> communityIdList) {
+    private static void getPossibleResultcommunityAssociation(
+            SparkSession spark,
+            List<String> allowedsemrel,
+            String outputPath,
+            List<String> communityIdList) {
 
         String communitylist = getConstraintList(" co.id = '", communityIdList);
-        String semrellist = getConstraintList(" relClass = '", allowedsemrel );
-
+        String semrellist = getConstraintList(" relClass = '", allowedsemrel);
 
         /*
         associates to each result the set of community contexts they are associated to
@@ -115,33 +134,38 @@ public class PrepareResultCommunitySetStep1 {
         associates to each target of a relation with allowed semantics the set of community context it could possibly
         inherit from the source of the relation
          */
-        String query = "Select target resultId, community_context  " +
-                "from (select id, collect_set(co.id) community_context " +
-                "       from  result " +
-                "       lateral view explode (context) c as co " +
-                "       where datainfo.deletedbyinference = false "+ communitylist +
-                "       group by id) p " +
-                "JOIN " +
-                "(select source, target " +
-                "from relation " +
-                "where datainfo.deletedbyinference = false " + semrellist + ") r " +
-                "ON p.id = r.source";
+        String query =
+                "Select target resultId, community_context  "
+                        + "from (select id, collect_set(co.id) community_context "
+                        + "       from  result "
+                        + "       lateral view explode (context) c as co "
+                        + "       where datainfo.deletedbyinference = false "
+                        + communitylist
+                        + "       group by id) p "
+                        + "JOIN "
+                        + "(select source, target "
+                        + "from relation "
+                        + "where datainfo.deletedbyinference = false "
+                        + semrellist
+                        + ") r "
+                        + "ON p.id = r.source";
 
-        org.apache.spark.sql.Dataset<Row> result_context = spark.sql( query);
+        org.apache.spark.sql.Dataset<Row> result_context = spark.sql(query);
         result_context.createOrReplaceTempView("result_context");
 
-        //( target, (mes, dh-ch-, ni))
+        // ( target, (mes, dh-ch-, ni))
         /*
         a dataset for example could be linked to more than one publication. For each publication linked to that dataset
-        the previous query will produce a row: targetId set of community context the te=arget could possibly inherit
+        the previous query will produce a row: targetId set of community context the target could possibly inherit
         with the following query there will be a single row for each result linked to more than one result of the result type
         currently being used
          */
-        query = "select resultId , collect_set(co) communityList " +
-                "from result_context " +
-                "lateral view explode (community_context) c as co " +
-                "where length(co) > 0 " +
-                "group by resultId";
+        query =
+                "select resultId , collect_set(co) communityList "
+                        + "from result_context "
+                        + "lateral view explode (community_context) c as co "
+                        + "where length(co) > 0 "
+                        + "group by resultId";
 
         spark.sql(query)
                 .as(Encoders.bean(ResultCommunityList.class))
--- dhp-workflows/dhp-propagation/src/main/java/eu/dnetlib/dhp/resulttocommunityfromsemrel/PrepareResultCommunitySetStep2.java
+++ dhp-workflows/dhp-propagation/src/main/java/eu/dnetlib/dhp/resulttocommunityfromsemrel/PrepareResultCommunitySetStep2.java
@@ -1,8 +1,13 @@
 package eu.dnetlib.dhp.resulttocommunityfromsemrel;
 
+import static eu.dnetlib.dhp.PropagationConstant.*;
+import static eu.dnetlib.dhp.common.SparkSessionSupport.runWithSparkSession;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import eu.dnetlib.dhp.application.ArgumentApplicationParser;
-import eu.dnetlib.dhp.orcidtoresultfromsemrel.ResultOrcidList;
 import eu.dnetlib.dhp.resulttocommunityfromorganization.ResultCommunityList;
+import java.util.HashSet;
+import java.util.Set;
 import org.apache.commons.io.IOUtils;
 import org.apache.hadoop.io.compress.GzipCodec;
 import org.apache.spark.SparkConf;
@@ -11,15 +16,7 @@ import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.SparkSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import scala.Tuple2;
-
-import java.util.HashSet;
-import java.util.Set;
-
-import static eu.dnetlib.dhp.PropagationConstant.*;
-import static eu.dnetlib.dhp.common.SparkSessionSupport.runWithSparkSession;
-
 
 public class PrepareResultCommunitySetStep2 {
     private static final Logger log = LoggerFactory.getLogger(PrepareResultCommunitySetStep2.class);
@@ -27,11 +24,12 @@ public class PrepareResultCommunitySetStep2 {
 
     public static void main(String[] args) throws Exception {
 
-        String jsonConfiguration = IOUtils.toString(PrepareResultCommunitySetStep2.class
-                .getResourceAsStream("/eu/dnetlib/dhp/resulttocommunityfromsemrel/input_prepareresulttocommunity2_parameters.json"));
+        String jsonConfiguration =
+                IOUtils.toString(
+                        PrepareResultCommunitySetStep2.class.getResourceAsStream(
+                                "/eu/dnetlib/dhp/resulttocommunityfromsemrel/input_preparecommunitytoresult2_parameters.json"));
 
-        final ArgumentApplicationParser parser = new ArgumentApplicationParser(
-                jsonConfiguration);
+        final ArgumentApplicationParser parser = new ArgumentApplicationParser(jsonConfiguration);
 
         parser.parseArgument(args);
 
@@ -46,60 +44,61 @@ public class PrepareResultCommunitySetStep2 {
 
         SparkConf conf = new SparkConf();
 
-        runWithSparkSession(conf, isSparkSessionManaged,
+        runWithSparkSession(
+                conf,
+                isSparkSessionManaged,
                 spark -> {
                     if (isTest(parser)) {
                         removeOutputDir(spark, outputPath);
                     }
-                    mergeInfo(spark,  inputPath, outputPath);
+                    mergeInfo(spark, inputPath, outputPath);
                 });
-
     }
 
     private static void mergeInfo(SparkSession spark, String inputPath, String outputPath) {
 
-        Dataset<ResultCommunityList> resultOrcidAssocCommunityList = readResultCommunityList(spark, inputPath + "/publication")
-                .union(readResultCommunityList(spark, inputPath + "/dataset"))
-                .union(readResultCommunityList(spark, inputPath + "/otherresearchproduct"))
-                .union(readResultCommunityList(spark, inputPath + "/software"));
+        Dataset<ResultCommunityList> resultOrcidAssocCommunityList =
+                readResultCommunityList(spark, inputPath + "/publication")
+                        .union(readResultCommunityList(spark, inputPath + "/dataset"))
+                        .union(readResultCommunityList(spark, inputPath + "/otherresearchproduct"))
+                        .union(readResultCommunityList(spark, inputPath + "/software"));
 
         resultOrcidAssocCommunityList
                 .toJavaRDD()
                 .mapToPair(r -> new Tuple2<>(r.getResultId(), r))
-                .reduceByKey((a, b) -> {
-                    if (a == null) {
-                        return b;
-                    }
-                    if (b == null) {
-                        return a;
-                    }
-                    Set<String> community_set = new HashSet<>();
+                .reduceByKey(
+                        (a, b) -> {
+                            if (a == null) {
+                                return b;
+                            }
+                            if (b == null) {
+                                return a;
+                            }
+                            Set<String> community_set = new HashSet<>();
 
-                    a.getCommunityList().stream().forEach(aa -> community_set.add(aa));
+                            a.getCommunityList().stream().forEach(aa -> community_set.add(aa));
 
-                    b.getCommunityList().stream().forEach(aa -> {
-                        if (!community_set.contains(aa)) {
-                            a.getCommunityList().add(aa);
-                            community_set.add(aa);
-                        }
-                    });
-                    return a;
-                })
+                            b.getCommunityList().stream()
+                                    .forEach(
+                                            aa -> {
+                                                if (!community_set.contains(aa)) {
+                                                    a.getCommunityList().add(aa);
+                                                    community_set.add(aa);
+                                                }
+                                            });
+                            return a;
+                        })
                 .map(c -> c._2())
                 .map(r -> OBJECT_MAPPER.writeValueAsString(r))
                 .saveAsTextFile(outputPath, GzipCodec.class);
     }
 
-    private static Dataset<ResultCommunityList> readResultCommunityList(SparkSession spark, String relationPath) {
-        return spark
-                .read()
+    private static Dataset<ResultCommunityList> readResultCommunityList(
+            SparkSession spark, String relationPath) {
+        return spark.read()
                 .textFile(relationPath)
-                .map(value -> OBJECT_MAPPER.readValue(value, ResultCommunityList.class), Encoders.bean(ResultCommunityList.class));
+                .map(
+                        value -> OBJECT_MAPPER.readValue(value, ResultCommunityList.class),
+                        Encoders.bean(ResultCommunityList.class));
     }
-
-
-
-
-
 }
-
