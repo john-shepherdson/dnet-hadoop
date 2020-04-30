564e5d6279f0d69637de82fcff5732961b8852df
miriam.baglioni <miriam.baglioni@isti.cnr.it>
2020-04-30 10:22:58 +0200

    added new information in support of blacklist reader

--- dhp-schemas/src/main/java/eu/dnetlib/dhp/schema/common/RelationInverse.java
+++ dhp-schemas/src/main/java/eu/dnetlib/dhp/schema/common/RelationInverse.java
@@ -1,46 +1,46 @@
+
 package eu.dnetlib.dhp.schema.common;
 
-public class RelationInverse  {
-    private String relation;
-    private String inverse;
-    private String relType;
-    private String subReltype;
+public class RelationInverse {
+	private String relation;
+	private String inverse;
+	private String relType;
+	private String subReltype;
 
-    public String getRelType() {
-        return relType;
-    }
+	public String getRelType() {
+		return relType;
+	}
 
-    public RelationInverse setRelType(String relType) {
-        this.relType = relType;
-        return this;
-    }
+	public RelationInverse setRelType(String relType) {
+		this.relType = relType;
+		return this;
+	}
 
-    public String getSubReltype() {
-        return subReltype;
-    }
+	public String getSubReltype() {
+		return subReltype;
+	}
 
-    public RelationInverse setSubReltype(String subReltype) {
-        this.subReltype = subReltype;
-        return this;
-    }
+	public RelationInverse setSubReltype(String subReltype) {
+		this.subReltype = subReltype;
+		return this;
+	}
 
-    public String getRelation() {
-        return relation;
-    }
+	public String getRelation() {
+		return relation;
+	}
 
-    public RelationInverse setRelation(String relation) {
-        this.relation = relation;
-        return this;
-    }
+	public RelationInverse setRelation(String relation) {
+		this.relation = relation;
+		return this;
+	}
 
-    public String getInverse() {
-        return inverse;
-    }
+	public String getInverse() {
+		return inverse;
+	}
 
-    public RelationInverse setInverse(String inverse) {
-        this.inverse = inverse;
-        return this;
-    }
-
+	public RelationInverse setInverse(String inverse) {
+		this.inverse = inverse;
+		return this;
+	}
 
 }
