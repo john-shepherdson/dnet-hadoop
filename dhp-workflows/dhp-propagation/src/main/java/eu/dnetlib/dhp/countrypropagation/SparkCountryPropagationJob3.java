5b3f8a0e909e020d5fe0b7aba89c798cc22854c8
Claudio Atzori <claudio.atzori@isti.cnr.it>
2020-05-07 11:41:41 +0200

    using Encoders.bean instead of kryo

--- dhp-workflows/dhp-propagation/src/main/java/eu/dnetlib/dhp/countrypropagation/SparkCountryPropagationJob3.java
+++ dhp-workflows/dhp-propagation/src/main/java/eu/dnetlib/dhp/countrypropagation/SparkCountryPropagationJob3.java
@@ -71,20 +71,15 @@ public class SparkCountryPropagationJob3 {
 		Class<? extends Result> resultClazz = (Class<? extends Result>) Class.forName(resultClassName);
 
 		SparkConf conf = new SparkConf();
-		conf.registerKryoClasses(ModelSupport.getOafModelClasses());
-
 		runWithSparkSession(
 			conf,
 			isSparkSessionManaged,
-			spark -> {
-
-				execPropagation(
-					spark,
-					inputPath,
-					outputPath,
-					resultClazz,
-					saveGraph);
-			});
+			spark -> execPropagation(
+				spark,
+				inputPath,
+				outputPath,
+				resultClazz,
+				saveGraph));
 	}
 
 	private static <R extends Result> void execPropagation(
@@ -101,7 +96,7 @@ public class SparkCountryPropagationJob3 {
 			spark
 				.read()
 				.json(inputPath)
-				.as(Encoders.kryo(resultClazz))
+				.as(Encoders.bean(resultClazz))
 				.groupByKey((MapFunction<R, String>) result1 -> result1.getId(), Encoders.STRING())
 				.mapGroups(getCountryMergeFn(resultClazz), Encoders.bean(resultClazz))
 				.write()
