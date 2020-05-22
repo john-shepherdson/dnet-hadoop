eb606dc1e298f397b69464eb98b96fb5c9374aea
michele.artini <michele.artini@isti.cnr.it>
2020-05-22 17:17:41 +0200

    partial implementation of events with rels

--- dhp-workflows/dhp-broker-events/src/main/java/eu/dnetlib/dhp/broker/oa/matchers/simple/EnrichMissingPublicationDate.java
+++ dhp-workflows/dhp-broker-events/src/main/java/eu/dnetlib/dhp/broker/oa/matchers/simple/EnrichMissingPublicationDate.java
@@ -1,11 +1,12 @@
 
-package eu.dnetlib.dhp.broker.oa.matchers;
+package eu.dnetlib.dhp.broker.oa.matchers.simple;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import eu.dnetlib.dhp.broker.model.Topic;
+import eu.dnetlib.dhp.broker.oa.matchers.UpdateMatcher;
 import eu.dnetlib.dhp.broker.oa.util.UpdateInfo;
 import eu.dnetlib.dhp.schema.oaf.Result;
 
--- dhp-workflows/dhp-broker-events/src/main/java/eu/dnetlib/dhp/broker/oa/matchers/simple/EnrichMissingSubject.java
+++ dhp-workflows/dhp-broker-events/src/main/java/eu/dnetlib/dhp/broker/oa/matchers/simple/EnrichMissingSubject.java
@@ -1,5 +1,5 @@
 
-package eu.dnetlib.dhp.broker.oa.matchers;
+package eu.dnetlib.dhp.broker.oa.matchers.simple;
 
 import java.util.List;
 import java.util.Set;
@@ -8,28 +8,32 @@ import java.util.stream.Collectors;
 import org.apache.commons.lang3.tuple.Pair;
 
 import eu.dnetlib.dhp.broker.model.Topic;
+import eu.dnetlib.dhp.broker.oa.matchers.UpdateMatcher;
 import eu.dnetlib.dhp.broker.oa.util.ConversionUtils;
 import eu.dnetlib.dhp.broker.oa.util.UpdateInfo;
+import eu.dnetlib.dhp.schema.oaf.Qualifier;
 import eu.dnetlib.dhp.schema.oaf.Result;
+import eu.dnetlib.dhp.schema.oaf.StructuredProperty;
 
-public class EnrichMoreSubject extends UpdateMatcher<Result, Pair<String, String>> {
+public class EnrichMissingSubject extends UpdateMatcher<Result, Pair<String, String>> {
 
-	public EnrichMoreSubject() {
+	public EnrichMissingSubject() {
 		super(true);
 	}
 
 	@Override
 	protected List<UpdateInfo<Pair<String, String>>> findUpdates(final Result source, final Result target) {
-		final Set<String> existingSubjects = target
+		final Set<String> existingTypes = target
 			.getSubject()
 			.stream()
-			.map(pid -> pid.getQualifier().getClassid() + "::" + pid.getValue())
+			.map(StructuredProperty::getQualifier)
+			.map(Qualifier::getClassid)
 			.collect(Collectors.toSet());
 
 		return source
 			.getPid()
 			.stream()
-			.filter(pid -> !existingSubjects.contains(pid.getQualifier().getClassid() + "::" + pid.getValue()))
+			.filter(pid -> !existingTypes.contains(pid.getQualifier().getClassid()))
 			.map(ConversionUtils::oafSubjectToPair)
 			.map(i -> generateUpdateInfo(i, source, target))
 			.collect(Collectors.toList());
@@ -41,7 +45,7 @@ public class EnrichMoreSubject extends UpdateMatcher<Result, Pair<String, String
 		final Result target) {
 
 		return new UpdateInfo<>(
-			Topic.fromPath("ENRICH/MORE/SUBJECT/" + highlightValue.getLeft()),
+			Topic.fromPath("ENRICH/MISSING/SUBJECT/" + highlightValue.getLeft()),
 			highlightValue, source, target,
 			(p, pair) -> p.getSubjects().add(pair.getRight()),
 			pair -> pair.getLeft() + "::" + pair.getRight());
