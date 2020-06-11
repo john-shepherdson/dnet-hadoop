c2e1b66e83b6bda2cdf863a1c10e6e02411baea1
michele.artini <michele.artini@isti.cnr.it>
2020-06-11 14:28:03 +0200

    Revert "orcid events"
    This reverts commit 48959e9a1710f627a902dc986e07e180193edb9e.

--- dhp-workflows/dhp-broker-events/src/main/java/eu/dnetlib/dhp/broker/oa/matchers/simple/EnrichMissingAuthorOrcid.java
+++ dhp-workflows/dhp-broker-events/src/main/java/eu/dnetlib/dhp/broker/oa/matchers/simple/EnrichMissingAuthorOrcid.java
@@ -1,68 +1,41 @@
 
 package eu.dnetlib.dhp.broker.oa.matchers.simple;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.tuple.Pair;
 
 import eu.dnetlib.dhp.broker.model.Topic;
 import eu.dnetlib.dhp.broker.oa.matchers.UpdateMatcher;
 import eu.dnetlib.dhp.broker.oa.util.UpdateInfo;
 import eu.dnetlib.dhp.broker.oa.util.aggregators.withRels.ResultWithRelations;
-import eu.dnetlib.dhp.schema.oaf.Author;
-import eu.dnetlib.dhp.schema.oaf.StructuredProperty;
 import eu.dnetlib.pace.config.DedupConfig;
 
-public class EnrichMissingAuthorOrcid extends UpdateMatcher<String> {
+public class EnrichMissingAuthorOrcid extends UpdateMatcher<Pair<String, String>> {
 
 	public EnrichMissingAuthorOrcid() {
 		super(true);
 	}
 
 	@Override
-	protected List<UpdateInfo<String>> findUpdates(final ResultWithRelations source,
+	protected List<UpdateInfo<Pair<String, String>>> findUpdates(final ResultWithRelations source,
 		final ResultWithRelations target,
 		final DedupConfig dedupConfig) {
-
-		final Set<String> existingOrcids = target
-			.getResult()
-			.getAuthor()
-			.stream()
-			.map(Author::getPid)
-			.flatMap(List::stream)
-			.filter(pid -> pid.getQualifier().getClassid().equalsIgnoreCase("orcid"))
-			.map(pid -> pid.getValue())
-			.collect(Collectors.toSet());
-
-		final List<UpdateInfo<String>> list = new ArrayList<>();
-
-		for (final Author author : source.getResult().getAuthor()) {
-			final String name = author.getFullname();
-
-			for (final StructuredProperty pid : author.getPid()) {
-				if (pid.getQualifier().getClassid().equalsIgnoreCase("orcid")
-					&& !existingOrcids.contains(pid.getValue())) {
-					list
-						.add(
-							generateUpdateInfo(name + " [ORCID: " + pid.getValue() + "]", source, target, dedupConfig));
-					;
-				}
-			}
-		}
-
-		return list;
+		// TODO
+		// return Arrays.asList(new EnrichMissingAbstract("xxxxxxx", 0.9f));
+		return Arrays.asList();
 	}
 
-	public UpdateInfo<String> generateUpdateInfo(final String highlightValue,
+	public UpdateInfo<Pair<String, String>> generateUpdateInfo(final Pair<String, String> highlightValue,
 		final ResultWithRelations source,
 		final ResultWithRelations target,
 		final DedupConfig dedupConfig) {
 		return new UpdateInfo<>(
 			Topic.ENRICH_MISSING_AUTHOR_ORCID,
 			highlightValue, source, target,
-			(p, aut) -> p.getCreators().add(aut),
-			aut -> aut,
+			(p, pair) -> p.getCreators().add(pair.getLeft() + " - ORCID: " + pair.getRight()),
+			pair -> pair.getLeft() + "::" + pair.getRight(),
 			dedupConfig);
 	}
 }
