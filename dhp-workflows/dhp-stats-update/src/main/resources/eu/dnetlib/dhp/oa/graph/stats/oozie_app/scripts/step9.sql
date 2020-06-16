5546f29e63ee454621d2501aaa6226f6e07b346e
Spyros Zoupanos <spyros@zoupanos.net>
2020-06-16 22:30:53 +0300

    Corrections on the shadow schema and the impala table stats calculation

--- dhp-workflows/dhp-stats-update/src/main/resources/eu/dnetlib/dhp/oa/graph/stats/oozie_app/scripts/step9.sql
+++ dhp-workflows/dhp-stats-update/src/main/resources/eu/dnetlib/dhp/oa/graph/stats/oozie_app/scripts/step9.sql
@@ -5,7 +5,7 @@
 ----------------------------------------------------------------
 DROP TABLE IF EXISTS ${stats_db_name}.organization;
 CREATE TABLE IF NOT EXISTS ${stats_db_name}.organization AS SELECT substr(o.id, 4) as id, o.legalname.value as name, o.legalshortname as legalshortname, o.country.classid as country 
-FROM openaire.organization o WHERE o.datainfo.deletedbyinference=FALSE;
+FROM ${openaire_db_name}.organization o WHERE o.datainfo.deletedbyinference=FALSE;
 
 CREATE OR REPLACE VIEW ${stats_db_name}.organization_datasources AS SELECT organization AS id, id AS datasource FROM ${stats_db_name}.datasource_organizations;
 
