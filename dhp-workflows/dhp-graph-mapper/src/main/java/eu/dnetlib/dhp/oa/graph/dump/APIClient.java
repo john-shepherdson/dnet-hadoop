fb803530181be906c8704a80bb0952eea75d28e8
miriam.baglioni <miriam.baglioni@isti.cnr.it>
2020-06-18 14:21:36 +0200

    -

--- dhp-workflows/dhp-graph-mapper/src/main/java/eu/dnetlib/dhp/oa/graph/dump/APIClient.java
+++ dhp-workflows/dhp-graph-mapper/src/main/java/eu/dnetlib/dhp/oa/graph/dump/APIClient.java
@@ -3,26 +3,20 @@ package eu.dnetlib.dhp.oa.graph.dump;
 
 import java.io.*;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.client.methods.RequestBuilder;
+
 import org.apache.http.entity.StringEntity;
-import org.apache.http.entity.mime.HttpMultipartMode;
+
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 
-// import com.cloudera.org.apache.http.HttpResponse;
-// import com.cloudera.org.apache.http.client.HttpClient;
-// import com.cloudera.org.apache.http.client.methods.HttpPost;
-// import com.cloudera.org.apache.http.entity.StringEntity;
-// import com.cloudera.org.apache.http.impl.client.DefaultHttpClient;
-// import com.cloudera.org.apache.http.util.EntityUtils;
 import com.google.gson.Gson;
 
 import eu.dnetlib.dhp.oa.graph.dump.zenodo.ZenodoModel;
@@ -31,7 +25,7 @@ public class APIClient implements Serializable {
 
 	String urlString;
 	String bucket;
-	HttpClient client;
+
 	String deposition_id;
 	final String ACCESS_TOKEN = "5ImUj0VC1ICg4ifK5dc3AGzJhcfAB4osxrFlsr8WxHXxjaYgCE0hY8HZcDoe";
 
@@ -61,7 +55,7 @@ public class APIClient implements Serializable {
 
 		String json = "{}";
 
-		client = new DefaultHttpClient();
+		HttpClient client = new DefaultHttpClient();
 
 		HttpPost post = new HttpPost(urlString);
 
@@ -80,22 +74,51 @@ public class APIClient implements Serializable {
 
 		return response.getStatusLine().getStatusCode();
 
+
 	}
 
 	public void upload(String filePath, String file_name) throws IOException {
+		HttpClient client = new DefaultHttpClient();
 		File file = new File(filePath);
-		HttpPut post = new HttpPut(bucket + "/" + file_name);
-		post.setHeader("Authorization", "Bearer " + ACCESS_TOKEN);
-		post.addHeader("Content-Type", "application/zip");
+		HttpPut put = new HttpPut(bucket + "/" + file_name);
+		put.setHeader("Authorization", "Bearer " + ACCESS_TOKEN);
+		put.addHeader("Content-Type", "application/zip");
 		HttpEntity data = MultipartEntityBuilder.create().addBinaryBody(file_name, file).build();
-		post.setEntity(data);
+		put.setEntity(data);
 
-		// HttpUriRequest request = RequestBuilder.post(bucket + "/" + file_name
-		// +"?access_token=5ImUj0VC1ICg4ifK5dc3AGzJhcfAB4osxrFlsr8WxHXxjaYgCE0hY8HZcDoe").setEntity(data).build();
+		HttpResponse response = client.execute(put);
+
+		String json = EntityUtils.toString(response.getEntity());
+
+		ZenodoModel newSubmission = new Gson().fromJson(json, ZenodoModel.class);
+
+		System.out.println(response.getStatusLine().getStatusCode());
+
+	}
+
+	public void sendMretadata(String metadata) throws IOException {
+
+		HttpClient client = new DefaultHttpClient();
+		HttpPut post = new HttpPut(urlString + "/" + deposition_id);
+		post.setHeader("Authorization", "Bearer " + ACCESS_TOKEN);
+		post.addHeader("Content-Type", "application/json");
+		StringEntity entity = new StringEntity(metadata, StandardCharsets.UTF_8);
+		post.setEntity(entity);
 
 		HttpResponse response = client.execute(post);
 		System.out.println(response.getStatusLine().getStatusCode());
 
+
+	}
+
+	public void publish() throws IOException {
+		HttpClient client = new DefaultHttpClient();
+		HttpPost post = new HttpPost(urlString +"/"+ deposition_id +"/actions/publish") ;
+		post.setHeader("Authorization", "Bearer " + ACCESS_TOKEN);
+
+		HttpResponse response = client.execute(post);
+
+		System.out.println(response.getStatusLine().getStatusCode());
 	}
 
 }
