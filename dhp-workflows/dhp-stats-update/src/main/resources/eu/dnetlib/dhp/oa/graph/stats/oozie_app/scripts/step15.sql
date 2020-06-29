ec52141f1a4d864083025abfb2ea0279f617fc43
antleb <50_David>
2020-06-30 01:49:56 +0300

    changed refereed type from value to clssname

--- dhp-workflows/dhp-stats-update/src/main/resources/eu/dnetlib/dhp/oa/graph/stats/oozie_app/scripts/step15.sql
+++ dhp-workflows/dhp-stats-update/src/main/resources/eu/dnetlib/dhp/oa/graph/stats/oozie_app/scripts/step15.sql
@@ -11,31 +11,31 @@
 -- When it is ready they should be added to the to statistic recalculation
 -- for the impala tables and the shadow views
 
--- CREATE TABLE IF NOT EXISTS ${stats_db_name}.publication_refereed as
--- select substr(r.id, 4) as id, inst.refereed.value as refereed
--- from ${openaire_db_name}.publication r lateral view explode(r.instance) instances as inst
--- where r.datainfo.deletedbyinference=false;
--- 
--- CREATE TABLE IF NOT EXISTS ${stats_db_name}.dataset_refereed as
--- select substr(r.id, 4) as id, inst.refereed.value as refereed
--- from ${openaire_db_name}.dataset r lateral view explode(r.instance) instances as inst
--- where r.datainfo.deletedbyinference=false;
--- 
--- CREATE TABLE IF NOT EXISTS ${stats_db_name}.software_refereed as
--- select substr(r.id, 4) as id, inst.refereed.value as refereed
--- from ${openaire_db_name}.software r lateral view explode(r.instance) instances as inst
--- where r.datainfo.deletedbyinference=false;
--- 
--- CREATE TABLE IF NOT EXISTS ${stats_db_name}.otherresearchproduct_refereed as
--- select substr(r.id, 4) as id, inst.refereed.value as refereed
--- from ${openaire_db_name}.otherresearchproduct r lateral view explode(r.instance) instances as inst
--- where r.datainfo.deletedbyinference=false;
--- 
--- CREATE VIEW IF NOT EXISTS ${stats_db_name}.result_refereed as
--- select * from ${stats_db_name}.publication_refereed
--- union all 
--- select * from ${stats_db_name}.dataset_refereed
--- union all
--- select * from ${stats_db_name}.software_refereed
--- union all
--- select * from ${stats_db_name}.otherresearchproduct_refereed;
+CREATE TABLE IF NOT EXISTS ${stats_db_name}.publication_refereed as
+select substr(r.id, 4) as id, inst.refereed.classsname as refereed
+from ${openaire_db_name}.publication r lateral view explode(r.instance) instances as inst
+where r.datainfo.deletedbyinference=false;
+
+CREATE TABLE IF NOT EXISTS ${stats_db_name}.dataset_refereed as
+select substr(r.id, 4) as id, inst.refereed.classsname as refereed
+from ${openaire_db_name}.dataset r lateral view explode(r.instance) instances as inst
+where r.datainfo.deletedbyinference=false;
+
+CREATE TABLE IF NOT EXISTS ${stats_db_name}.software_refereed as
+select substr(r.id, 4) as id, inst.refereed.classsname as refereed
+from ${openaire_db_name}.software r lateral view explode(r.instance) instances as inst
+where r.datainfo.deletedbyinference=false;
+
+CREATE TABLE IF NOT EXISTS ${stats_db_name}.otherresearchproduct_refereed as
+select substr(r.id, 4) as id, inst.refereed.classsname as refereed
+from ${openaire_db_name}.otherresearchproduct r lateral view explode(r.instance) instances as inst
+where r.datainfo.deletedbyinference=false;
+
+CREATE VIEW IF NOT EXISTS ${stats_db_name}.result_refereed as
+select * from ${stats_db_name}.publication_refereed
+union all
+select * from ${stats_db_name}.dataset_refereed
+union all
+select * from ${stats_db_name}.software_refereed
+union all
+select * from ${stats_db_name}.otherresearchproduct_refereed;
