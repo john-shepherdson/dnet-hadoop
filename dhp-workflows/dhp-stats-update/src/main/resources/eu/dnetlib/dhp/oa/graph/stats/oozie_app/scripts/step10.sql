cecbbfa0fc00991357c1d303233173110d5e2a76
antleb <antleb@di.uoa.gr>
2020-07-06 15:16:58 +0300

    added missing tables and views: contexts, creation_date, funder

--- dhp-workflows/dhp-stats-update/src/main/resources/eu/dnetlib/dhp/oa/graph/stats/oozie_app/scripts/step10.sql
+++ dhp-workflows/dhp-stats-update/src/main/resources/eu/dnetlib/dhp/oa/graph/stats/oozie_app/scripts/step10.sql
@@ -8,3 +8,14 @@ CREATE OR REPLACE VIEW  ${stats_db_name}.country AS SELECT * FROM ${external_sta
 CREATE OR REPLACE VIEW  ${stats_db_name}.countrygdp AS SELECT * FROM ${external_stats_db_name}.countrygdp;
 CREATE OR REPLACE VIEW  ${stats_db_name}.roarmap AS SELECT * FROM ${external_stats_db_name}.roarmap;
 CREATE OR REPLACE VIEW  ${stats_db_name}.rndexpediture AS SELECT * FROM ${external_stats_db_name}.rndexpediture;
+CREATE OR REPLACE VIEW  ${stats_db_name}.context AS SELECT * FROM ${external_stats_db_name}.context;
+CREATE OR REPLACE VIEW  ${stats_db_name}.category AS SELECT * FROM ${external_stats_db_name}.category;
+CREATE OR REPLACE VIEW  ${stats_db_name}.concept AS SELECT * FROM ${external_stats_db_name}.concept;
+
+
+------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------
+-- Creation date of the database
+------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------
+create table creation_date as select date_format(current_date(), 'dd-MM-yyyy') as date;
