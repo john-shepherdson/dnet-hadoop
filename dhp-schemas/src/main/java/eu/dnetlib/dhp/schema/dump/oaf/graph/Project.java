a2f01e52590965592816ba2f2dd21c672a99cc57
miriam.baglioni <miriam.baglioni@isti.cnr.it>
2020-07-20 18:54:17 +0200

    added getter and setter

--- dhp-schemas/src/main/java/eu/dnetlib/dhp/schema/dump/oaf/graph/Project.java
+++ dhp-schemas/src/main/java/eu/dnetlib/dhp/schema/dump/oaf/graph/Project.java
@@ -35,4 +35,139 @@ public class Project implements Serializable {
 
     private Programme programme;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getWebsiteurl() {
+        return websiteurl;
+    }
+
+    public void setWebsiteurl(String websiteurl) {
+        this.websiteurl = websiteurl;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getAcronym() {
+        return acronym;
+    }
+
+    public void setAcronym(String acronym) {
+        this.acronym = acronym;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStartdate() {
+        return startdate;
+    }
+
+    public void setStartdate(String startdate) {
+        this.startdate = startdate;
+    }
+
+    public String getEnddate() {
+        return enddate;
+    }
+
+    public void setEnddate(String enddate) {
+        this.enddate = enddate;
+    }
+
+    public String getCallidentifier() {
+        return callidentifier;
+    }
+
+    public void setCallidentifier(String callidentifier) {
+        this.callidentifier = callidentifier;
+    }
+
+    public String getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public boolean isOpenaccessmandateforpublications() {
+        return openaccessmandateforpublications;
+    }
+
+    public void setOpenaccessmandateforpublications(boolean openaccessmandateforpublications) {
+        this.openaccessmandateforpublications = openaccessmandateforpublications;
+    }
+
+    public boolean isOpenaccessmandatefordataset() {
+        return openaccessmandatefordataset;
+    }
+
+    public void setOpenaccessmandatefordataset(boolean openaccessmandatefordataset) {
+        this.openaccessmandatefordataset = openaccessmandatefordataset;
+    }
+
+    public List<String> getSubject() {
+        return subject;
+    }
+
+    public void setSubject(List<String> subject) {
+        this.subject = subject;
+    }
+
+    public Funder getFunding() {
+        return funding;
+    }
+
+    public void setFunding(Funder funding) {
+        this.funding = funding;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public Granted getGranted() {
+        return granted;
+    }
+
+    public void setGranted(Granted granted) {
+        this.granted = granted;
+    }
+
+    public Programme getProgramme() {
+        return programme;
+    }
+
+    public void setProgramme(Programme programme) {
+        this.programme = programme;
+    }
 }
