86bab795127a93da0dceb18d65011002f84da447
miriam.baglioni <miriam.baglioni@isti.cnr.it>
2020-07-29 18:20:22 +0200

    -

--- dhp-workflows/dhp-graph-mapper/src/main/java/eu/dnetlib/dhp/oa/graph/dump/graph/Constants.java
+++ dhp-workflows/dhp-graph-mapper/src/main/java/eu/dnetlib/dhp/oa/graph/dump/graph/Constants.java
@@ -27,5 +27,5 @@ public class Constants implements Serializable {
 
 	public static final String HARVESTED = "Harvested";
 	public static final String DEFAULT_TRUST = "0.9";
-   // public static final String FUNDER_DS = "entityregistry::projects";
+	// public static final String FUNDER_DS = "entityregistry::projects";
 }
