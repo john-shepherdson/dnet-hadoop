ff7d05abb432ccf78dc62a77e560f70ed96a832a
miriam.baglioni <miriam.baglioni@isti.cnr.it>
2020-07-30 16:32:04 +0200

    added support class to store the couple organizationId representativeId gaot from sql query on hive

---
+++ dhp-workflows/dhp-graph-mapper/src/main/java/eu/dnetlib/dhp/oa/graph/dump/graph/MergedRels.java
@@ -0,0 +1,25 @@
+
+package eu.dnetlib.dhp.oa.graph.dump.graph;
+
+import java.io.Serializable;
+
+public class MergedRels implements Serializable {
+	private String organizationId;
+	private String representativeId;
+
+	public String getOrganizationId() {
+		return organizationId;
+	}
+
+	public void setOrganizationId(String organizationId) {
+		this.organizationId = organizationId;
+	}
+
+	public String getRepresentativeId() {
+		return representativeId;
+	}
+
+	public void setRepresentativeId(String representativeId) {
+		this.representativeId = representativeId;
+	}
+}
