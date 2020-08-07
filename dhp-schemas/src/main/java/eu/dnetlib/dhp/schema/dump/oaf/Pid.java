150dd27431319ea41d783fdf4af74fd692ea6e48
miriam.baglioni <miriam.baglioni@isti.cnr.it>
2020-08-07 15:29:13 +0200

    changed the name od the pid: from pid to id

--- dhp-schemas/src/main/java/eu/dnetlib/dhp/schema/dump/oaf/Pid.java
+++ dhp-schemas/src/main/java/eu/dnetlib/dhp/schema/dump/oaf/Pid.java
@@ -4,15 +4,15 @@ package eu.dnetlib.dhp.schema.dump.oaf;
 import java.io.Serializable;
 
 public class Pid implements Serializable {
-	private ControlledField pid;
+	private ControlledField id;
 	private Provenance provenance;
 
-	public ControlledField getPid() {
-		return pid;
+	public ControlledField getId() {
+		return id;
 	}
 
-	public void setPid(ControlledField pid) {
-		this.pid = pid;
+	public void setId(ControlledField pid) {
+		this.id = pid;
 	}
 
 	public Provenance getProvenance() {
@@ -21,5 +21,20 @@ public class Pid implements Serializable {
 
 	public void setProvenance(Provenance provenance) {
 		this.provenance = provenance;
+	}
+
+	public static Pid newInstance(ControlledField pid, Provenance provenance) {
+		Pid p = new Pid();
+		p.id = pid;
+		p.provenance = provenance;
+
+		return p;
+	}
+
+	public static Pid newInstance(ControlledField pid) {
+		Pid p = new Pid();
+		p.id = pid;
+
+		return p;
 	}
 }
