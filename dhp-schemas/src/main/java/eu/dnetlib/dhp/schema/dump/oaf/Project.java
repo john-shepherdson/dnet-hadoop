11b80899d76d6c7b4212837a94e7bbc18801f6f3
miriam.baglioni <miriam.baglioni@isti.cnr.it>
2020-08-19 11:43:18 +0200

    added to store information concerning to project common to both the dumps for community and the whole graph

--- dhp-schemas/src/main/java/eu/dnetlib/dhp/schema/dump/oaf/Project.java
+++ dhp-schemas/src/main/java/eu/dnetlib/dhp/schema/dump/oaf/Project.java
@@ -3,6 +3,13 @@ package eu.dnetlib.dhp.schema.dump.oaf;
 
 import java.io.Serializable;
 
+/**
+ * This class to store the common information about the project that will be dumped for community and for the whole graph
+ * - private String id to store the id of the project (OpenAIRE id)
+ * - private String code to store the grant agreement of the project
+ * - private String acronym to store the acronym of the project
+ * - private String title to store the tile of the project
+ */
 public class Project implements Serializable {
 	protected String id;// OpenAIRE id
 	protected String code;
