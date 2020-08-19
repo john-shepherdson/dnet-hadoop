6b8c5034fc74c280cd41df23d2a72e5424dd3380
miriam.baglioni <miriam.baglioni@isti.cnr.it>
2020-08-19 11:46:20 +0200

    extends the result with parameters specific for the community dump.

--- dhp-schemas/src/main/java/eu/dnetlib/dhp/schema/dump/oaf/community/CommunityResult.java
+++ dhp-schemas/src/main/java/eu/dnetlib/dhp/schema/dump/oaf/community/CommunityResult.java
@@ -10,11 +10,13 @@ import eu.dnetlib.dhp.schema.dump.oaf.Result;
  * extends eu.dnetlib.dhp.schema.dump.oaf.Result with the following parameters: - projects of type
  * List<eu.dnetlib.dhp.schema.dump.oaf.community.Project> to store the list of projects related to the result. The
  * information is added after the result is mapped to the external model - context of type
- * List<eu.dnetlib.dhp.schema/dump.oaf.community.Context> to store information about the RC RI related to the result.
+ * List<eu.dnetlib.dhp.schema.dump.oaf.community.Context> to store information about the RC RI related to the result.
  * For each context in the result represented in the internal model one context in the external model is produced -
  * collectedfrom of type List<eu.dnetliv.dhp.schema.dump.oaf.KeyValue> to store information about the sources from which
  * the record has been collected. For each collectedfrom in the result represented in the internal model one
  * collectedfrom in the external model is produced
+ * - instance of type List<eu.dnetlib.dhp.schema.dump.oaf.community.CommunityInstance> to store all the instances associated
+ * to the result. It corresponds to the same parameter in the result represented in the internal model
  */
 public class CommunityResult extends Result {
 
@@ -23,6 +25,17 @@ public class CommunityResult extends Result {
 	private List<Context> context;
 
 	protected List<KeyValue> collectedfrom;
+
+	private List<CommunityInstance> instance ;
+
+	public List<CommunityInstance> getInstance() {
+		return instance;
+	}
+
+	public void setInstance(List<CommunityInstance> instance) {
+		this.instance = instance;
+	}
+
 
 	public List<KeyValue> getCollectedfrom() {
 		return collectedfrom;
