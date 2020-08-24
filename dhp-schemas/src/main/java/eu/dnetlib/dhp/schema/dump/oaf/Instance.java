bd5a72929b72abe77d37e8259f54fc1e67cdc943
miriam.baglioni <miriam.baglioni@isti.cnr.it>
2020-08-24 17:03:06 +0200

    refactoring due to compilation

--- dhp-schemas/src/main/java/eu/dnetlib/dhp/schema/dump/oaf/Instance.java
+++ dhp-schemas/src/main/java/eu/dnetlib/dhp/schema/dump/oaf/Instance.java
@@ -6,19 +6,15 @@ import java.util.List;
 
 /**
  * Represents the manifestations (i.e. different versions) of the result. For example: the pre-print and the published
- * versions are two manifestations of the same research result. It has the following parameters:
- * - license of type
+ * versions are two manifestations of the same research result. It has the following parameters: - license of type
  * String to store the license applied to the instance. It corresponds to the value of the licence in the instance to be
- * dumped
- * - accessright of type eu.dnetlib.dhp.schema.dump.oaf.AccessRight to store the accessright of the instance.
- * - type of type String to store the type of the instance as defined in the corresponding dnet vocabulary
- * (dnet:pubication_resource). It corresponds to the instancetype.classname of the instance to be mapped
- * - url of type List<String> list of locations where the instance
- * is accessible. It corresponds to url of the instance to be dumped
- *  - publicationdate of type String to store the
- * publication date of the instance ;// dateofacceptance; - refereed of type String to store information abour tthe
- * review status of the instance. Possible values are 'Unknown', 'nonPeerReviewed', 'peerReviewed'. It corresponds to
- * refereed.classname of the instance to be dumped
+ * dumped - accessright of type eu.dnetlib.dhp.schema.dump.oaf.AccessRight to store the accessright of the instance. -
+ * type of type String to store the type of the instance as defined in the corresponding dnet vocabulary
+ * (dnet:pubication_resource). It corresponds to the instancetype.classname of the instance to be mapped - url of type
+ * List<String> list of locations where the instance is accessible. It corresponds to url of the instance to be dumped -
+ * publicationdate of type String to store the publication date of the instance ;// dateofacceptance; - refereed of type
+ * String to store information abour tthe review status of the instance. Possible values are 'Unknown',
+ * 'nonPeerReviewed', 'peerReviewed'. It corresponds to refereed.classname of the instance to be dumped
  */
 public class Instance implements Serializable {
 
