f747e303acb3ffc6ae272a5285666ebde5e0690d
miriam.baglioni <miriam.baglioni@isti.cnr.it>
2020-10-30 14:13:45 +0100

    classes for dumping of the graph as ttl file

--- dhp-workflows/dhp-graph-mapper/src/main/java/eu/dnetlib/dhp/oa/graph/dump/ttl/OrganizationInfo.java
+++ dhp-workflows/dhp-graph-mapper/src/main/java/eu/dnetlib/dhp/oa/graph/dump/ttl/OrganizationInfo.java
@@ -1,4 +1,62 @@
+
 package eu.dnetlib.dhp.oa.graph.dump.ttl;
 
-public class OrganizationInfo {
+import java.io.Serializable;
+import java.util.List;
+
+public class OrganizationInfo implements Serializable {
+	private String name;
+	private String shortName;
+	private String country;
+	private String websiteUrl;
+	private List<Pids> pidsList;
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getShortName() {
+		return shortName;
+	}
+
+	public void setShortName(String shortName) {
+		this.shortName = shortName;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getWebsiteUrl() {
+		return websiteUrl;
+	}
+
+	public void setWebsiteUrl(String webciteUrl) {
+		this.websiteUrl = webciteUrl;
+	}
+
+	public List<Pids> getPidsList() {
+		return pidsList;
+	}
+
+	public void setPidsList(List<Pids> pidsList) {
+		this.pidsList = pidsList;
+	}
 }
--- dhp-workflows/dhp-graph-mapper/src/main/java/eu/dnetlib/dhp/oa/graph/dump/ttl/Pids.java
+++ dhp-workflows/dhp-graph-mapper/src/main/java/eu/dnetlib/dhp/oa/graph/dump/ttl/Pids.java
@@ -1,4 +1,25 @@
+
 package eu.dnetlib.dhp.oa.graph.dump.ttl;
 
-public class Pids {
+import java.io.Serializable;
+
+public class Pids implements Serializable {
+	private String type;
+	private String value;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
 }
