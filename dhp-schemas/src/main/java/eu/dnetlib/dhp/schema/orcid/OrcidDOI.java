302baab67b3de2e8f186a001ebc69e712058649a
Sandro La Bruzzo <sandro.labruzzo@isti.cnr.it>
2020-12-07 19:59:33 +0100

    fixed doiboost mapping and workflows

--- dhp-schemas/src/main/java/eu/dnetlib/dhp/schema/orcid/OrcidDOI.java
+++ dhp-schemas/src/main/java/eu/dnetlib/dhp/schema/orcid/OrcidDOI.java
@@ -1,24 +1,25 @@
+
 package eu.dnetlib.dhp.schema.orcid;
 
 import java.util.List;
 
 public class OrcidDOI {
-    private String doi;
-    private List<AuthorData> authors;
+	private String doi;
+	private List<AuthorData> authors;
 
-    public String getDoi() {
-        return doi;
-    }
+	public String getDoi() {
+		return doi;
+	}
 
-    public void setDoi(String doi) {
-        this.doi = doi;
-    }
+	public void setDoi(String doi) {
+		this.doi = doi;
+	}
 
-    public List<AuthorData> getAuthors() {
-        return authors;
-    }
+	public List<AuthorData> getAuthors() {
+		return authors;
+	}
 
-    public void setAuthors(List<AuthorData> authors) {
-        this.authors = authors;
-    }
+	public void setAuthors(List<AuthorData> authors) {
+		this.authors = authors;
+	}
 }
