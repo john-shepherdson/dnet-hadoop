6fbc67a9595564c5df9976e0751016c4843317c7
miriam.baglioni <miriam.baglioni@isti.cnr.it>
2020-12-09 17:10:20 +0100

    using ModelConstant.ORCID and removing not used constants

--- dhp-common/src/main/java/eu/dnetlib/dhp/common/Constants.java
+++ dhp-common/src/main/java/eu/dnetlib/dhp/common/Constants.java
@@ -10,21 +10,7 @@ public class Constants {
 	public static final Map<String, String> accessRightsCoarMap = Maps.newHashMap();
 	public static final Map<String, String> coarCodeLabelMap = Maps.newHashMap();
 
-	public static final String INFERRED = "Inferred by OpenAIRE";
-
-	public static final String HARVESTED = "Harvested";
-	public static final String DEFAULT_TRUST = "0.9";
-	public static final String USER_CLAIM = "Linked by user";;
-
 	public static String COAR_ACCESS_RIGHT_SCHEMA = "http://vocabularies.coar-repositories.org/documentation/access_rights/";
-
-	public static String ZENODO_COMMUNITY_PREFIX = "https://zenodo.org/communities/";
-
-	public static String RESEARCH_COMMUNITY = "Research Community";
-
-	public static String RESEARCH_INFRASTRUCTURE = "Research Infrastructure/Initiative";
-
-	public static String ORCID = "orcid";
 
 	static {
 		accessRightsCoarMap.put("OPEN", "c_abf2");
