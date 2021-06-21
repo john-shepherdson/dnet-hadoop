507e42102a7bd697dcf9fcf8fd3b86e5a039aa5e
Sandro La Bruzzo <sandro.labruzzo@isti.cnr.it>
2021-06-21 09:36:40 +0200

    added pdb to oaf class

--- dhp-workflows/dhp-aggregation/src/test/java/eu/dnetlib/dhp/aggregation/AbstractVocabularyTest.java
+++ dhp-workflows/dhp-aggregation/src/test/java/eu/dnetlib/dhp/aggregation/AbstractVocabularyTest.java
@@ -6,13 +6,13 @@ import static org.mockito.Mockito.lenient;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 import org.apache.commons.io.IOUtils;
 import org.mockito.Mock;
 
 import eu.dnetlib.dhp.common.vocabulary.VocabularyGroup;
 import eu.dnetlib.dhp.transformation.TransformationFactory;
-import eu.dnetlib.dhp.transformation.TransformationJobTest;
 import eu.dnetlib.enabling.is.lookup.rmi.ISLookUpException;
 import eu.dnetlib.enabling.is.lookup.rmi.ISLookUpService;
 
@@ -34,16 +34,22 @@ public abstract class AbstractVocabularyTest {
 
 	private static List<String> vocs() throws IOException {
 		return IOUtils
-			.readLines(TransformationJobTest.class.getResourceAsStream("/eu/dnetlib/dhp/transform/terms.txt"));
+			.readLines(
+				Objects
+					.requireNonNull(
+						AbstractVocabularyTest.class.getResourceAsStream("/eu/dnetlib/dhp/transform/terms.txt")));
 	}
 
 	private static List<String> synonyms() throws IOException {
 		return IOUtils
-			.readLines(TransformationJobTest.class.getResourceAsStream("/eu/dnetlib/dhp/transform/synonyms.txt"));
+			.readLines(
+				Objects
+					.requireNonNull(
+						AbstractVocabularyTest.class.getResourceAsStream("/eu/dnetlib/dhp/transform/synonyms.txt")));
 	}
 
 	protected void mockupTrasformationRule(final String trule, final String path) throws Exception {
-		final String trValue = IOUtils.toString(this.getClass().getResourceAsStream(path));
+		final String trValue = IOUtils.toString(Objects.requireNonNull(this.getClass().getResourceAsStream(path)));
 
 		lenient()
 			.when(isLookUpService.quickSearchProfile(String.format(TransformationFactory.TRULE_XQUERY, trule)))
--- dhp-workflows/dhp-graph-mapper/src/main/java/eu/dnetlib/dhp/sx/bio/PDBToOAF.scala
+++ dhp-workflows/dhp-graph-mapper/src/main/java/eu/dnetlib/dhp/sx/bio/PDBToOAF.scala
@@ -1,13 +1,14 @@
 package eu.dnetlib.dhp.sx.bio
 
+import eu.dnetlib.dhp.schema.common.ModelConstants
 import eu.dnetlib.dhp.schema.oaf.utils.OafMapperUtils
-import eu.dnetlib.dhp.schema.oaf.{Dataset, Oaf}
+import eu.dnetlib.dhp.schema.oaf.{DataInfo, Dataset, Oaf}
 import org.json4s.DefaultFormats
-import org.json4s.JsonAST.{JField, JObject, JString}
 import org.json4s.jackson.JsonMethods.parse
-
+import scala.collection.JavaConverters._
 object PDBToOAF {
 
+  val dataInfo: DataInfo = OafMapperUtils.dataInfo(false, null, false, false, ModelConstants.PROVENANCE_ACTION_SET_QUALIFIER, "0.9")
   def convert(input:String):List[Oaf]= {
     implicit lazy val formats: DefaultFormats.type = org.json4s.DefaultFormats
     lazy val json = parse(input)
@@ -18,13 +19,16 @@ object PDBToOAF {
 
     val d = new Dataset
 
-    d.setPid(List(OafMapperUtils.structuredProperty()))
+    d.setPid(
+      List(
+        OafMapperUtils.structuredProperty(pdb, "pdb", "pdb", ModelConstants.DNET_PID_TYPES, ModelConstants.DNET_PID_TYPES,dataInfo)
+      ).asJava
+    )
 
 
 
 
-
-    List()
+    List(d)
 
   }
 
