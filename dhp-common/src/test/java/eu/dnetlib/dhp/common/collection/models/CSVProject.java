a5f6edfa6c2edf9ecff17e5781315fff20d024d0
miriam.baglioni <miriam.baglioni@isti.cnr.it>
2021-08-13 09:30:03 +0200

    GetCSV refactoring - changed to mirror the original model class

--- dhp-common/src/test/java/eu/dnetlib/dhp/common/collection/models/CSVProject.java
+++ dhp-common/src/test/java/eu/dnetlib/dhp/common/collection/models/CSVProject.java
@@ -13,23 +13,12 @@ public class CSVProject implements Serializable {
 	@CsvBindByName(column = "id")
 	private String id;
 
-	@CsvBindByName(column = "status")
-	private String status;
-
 	@CsvBindByName(column = "programme")
 	private String programme;
 
 	@CsvBindByName(column = "topics")
 	private String topics;
 
-	@CsvBindByName(column = "title")
-	private String title;
-
-	@CsvBindByName(column = "call")
-	private String call;
-
-	@CsvBindByName(column = "subjects")
-	private String subjects;
 
 	public String getId() {
 		return id;
@@ -39,13 +28,7 @@ public class CSVProject implements Serializable {
 		this.id = id;
 	}
 
-	public String getStatus() {
-		return status;
-	}
 
-	public void setStatus(String status) {
-		this.status = status;
-	}
 
 	public String getProgramme() {
 		return programme;
@@ -63,28 +46,6 @@ public class CSVProject implements Serializable {
 		this.topics = topics;
 	}
 
-	public String getTitle() {
-		return title;
-	}
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getCall() {
-		return call;
-	}
-
-	public void setCall(String call) {
-		this.call = call;
-	}
-
-	public String getSubjects() {
-		return subjects;
-	}
-
-	public void setSubjects(String subjects) {
-		this.subjects = subjects;
-	}
 
 }
