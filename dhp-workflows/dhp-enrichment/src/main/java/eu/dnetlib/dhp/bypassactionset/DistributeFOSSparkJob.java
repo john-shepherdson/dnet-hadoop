a7d50c499bb069d030e0fbeec1cb4b4986981d98
miriam.baglioni <miriam.baglioni@isti.cnr.it>
2021-11-08 16:44:19 +0100

    [BypassAS] prepare FOS subject, test and model for FOS and BipFinder scores

---
+++ dhp-workflows/dhp-enrichment/src/main/java/eu/dnetlib/dhp/bypassactionset/DistributeFOSSparkJob.java
@@ -0,0 +1,84 @@
+package eu.dnetlib.dhp.bypassactionset;
+
+import eu.dnetlib.dhp.application.ArgumentApplicationParser;
+import eu.dnetlib.dhp.bypassactionset.model.FOSDataModel;
+import org.apache.commons.io.IOUtils;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.SaveMode;
+import org.apache.spark.sql.SparkSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static eu.dnetlib.dhp.PropagationConstant.*;
+import static eu.dnetlib.dhp.common.SparkSessionSupport.runWithSparkSession;
+
+public class DistributeFOSSparkJob implements Serializable {
+    private static final Logger log = LoggerFactory.getLogger(DistributeFOSSparkJob.class);
+
+    public static void main(String[] args) throws Exception {
+
+        String jsonConfiguration = IOUtils
+                .toString(
+                        DistributeFOSSparkJob.class
+                                .getResourceAsStream(
+                                        "/eu/dnetlib/dhp/bypassactionset/distribute_fos_parameters.json"));
+
+        final ArgumentApplicationParser parser = new ArgumentApplicationParser(jsonConfiguration);
+
+        parser.parseArgument(args);
+
+        Boolean isSparkSessionManaged = isSparkSessionManaged(parser);
+        log.info("isSparkSessionManaged: {}", isSparkSessionManaged);
+
+        String sourcePath = parser.get("sourcePath");
+        log.info("sourcePath: {}", sourcePath);
+
+
+
+        final String outputPath = parser.get("outputPath");
+        log.info("outputPath: {}", outputPath);
+
+
+
+        SparkConf conf = new SparkConf();
+        runWithSparkSession(
+                conf,
+                isSparkSessionManaged,
+                spark -> {
+                    removeOutputDir(spark, outputPath);
+                    distributeFOSdois(
+                            spark,
+                            sourcePath,
+
+                            outputPath
+                            );
+                });
+    }
+
+    private static void distributeFOSdois(SparkSession spark, String sourcePath, String outputPath) {
+        Dataset<FOSDataModel> fosDataset = readPath(spark, sourcePath, FOSDataModel.class);
+
+        fosDataset.flatMap((FlatMapFunction<FOSDataModel, FOSDataModel>) v -> {
+            List<FOSDataModel> fosList = new ArrayList<>();
+            final String level1 = v.getLevel1();
+            final String level2 = v.getLevel2();
+            final String level3 = v.getLevel3();
+            Arrays.stream(v.getDoi().split("\u0002")).forEach(d -> fosList.add(FOSDataModel.newInstance(d, level1, level2, level3)));
+            return fosList.iterator();
+        }, Encoders.bean(FOSDataModel.class))
+                .write()
+                .mode(SaveMode.Overwrite)
+                .option("compression","gzip")
+                .json(outputPath);
+    }
+
+
+}
---
+++ dhp-workflows/dhp-enrichment/src/main/java/eu/dnetlib/dhp/bypassactionset/GetFOSData.java
@@ -0,0 +1,79 @@
+package eu.dnetlib.dhp.bypassactionset;
+
+import eu.dnetlib.dhp.application.ArgumentApplicationParser;
+
+import eu.dnetlib.dhp.common.collection.GetCSV;
+import org.apache.commons.io.IOUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+
+import java.util.Objects;
+import java.util.Optional;
+
+public class GetFOSData implements Serializable {
+
+    private static final Logger log = LoggerFactory.getLogger(GetFOSData.class);
+
+    public static final char DEFAULT_DELIMITER = '\t';
+
+    public static void main(final String[] args) throws Exception {
+        final ArgumentApplicationParser parser = new ArgumentApplicationParser(
+                IOUtils
+                        .toString(
+                                Objects
+                                        .requireNonNull(
+                                                GetFOSData.class
+                                                        .getResourceAsStream(
+                                                                "/eu/dnetlib/dhp/bypassactionset/get_fos_parameters.json"))));
+
+        parser.parseArgument(args);
+
+        //the path where the original fos csv file is stored
+        final String inputPath = parser.get("inputPath");
+        log.info("inputPath {}", inputPath);
+
+        //the path where to put the file as json
+        final String outputFile = parser.get("outputFile");
+        log.info("outputFile {}", outputFile);
+
+        final String hdfsNameNode = parser.get("hdfsNameNode");
+        log.info("hdfsNameNode {}", hdfsNameNode);
+
+        final String classForName = parser.get("classForName");
+        log.info("classForName {}", classForName);
+
+        final char delimiter = Optional
+                .ofNullable(parser.get("delimiter"))
+                .map(s -> s.charAt(0))
+                .orElse(DEFAULT_DELIMITER);
+        log.info("delimiter {}", delimiter);
+
+        Configuration conf = new Configuration();
+        conf.set("fs.defaultFS", hdfsNameNode);
+
+        FileSystem fileSystem = FileSystem.get(conf);
+
+        new GetFOSData().doRewrite(inputPath, outputFile, classForName, delimiter, fileSystem);
+
+    }
+
+    public void doRewrite(String inputPath, String outputFile, String classForName, char delimiter, FileSystem fs)
+            throws IOException, ClassNotFoundException{
+
+        //reads the csv and writes it as its json equivalent
+        try (InputStreamReader reader = new InputStreamReader(fs.open(new Path (inputPath)))) {
+            GetCSV.getCsv(fs, reader, outputFile, classForName, delimiter);
+        }
+
+
+
+    }
+
+
+
+}
