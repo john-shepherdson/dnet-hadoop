01e5e0142ac9d0e181e1d62b03c564bf4c15953d
Claudio Atzori <claudio.atzori@isti.cnr.it>
2021-12-01 09:46:26 +0100

    added test to verify the relation inverse lookup operation

---
+++ dhp-workflows/dhp-blacklist/src/test/java/eu/dnetlib/dhp/blacklist/BlacklistRelationTest.java
@@ -0,0 +1,35 @@
+package eu.dnetlib.dhp.blacklist;
+
+import eu.dnetlib.dhp.schema.common.ModelSupport;
+import eu.dnetlib.dhp.schema.common.RelationInverse;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BlacklistRelationTest {
+
+    @Test
+    public void testRelationInverseLookup() {
+
+        final List<String> rels = Arrays.asList(
+             "resultResult_relationship_IsRelatedTo",
+             "resultOrganization_affiliation_isAuthorInstitutionOf",
+             "resultOrganization_affiliation_hasAuthorInstitution",
+             "datasourceOrganization_provision_isProvidedBy",
+             "projectOrganization_participation_hasParticipant",
+             "resultProject_outcome_produces",
+             "resultProject_outcome_isProducedBy");
+
+        rels.forEach(r -> {
+            RelationInverse inverse = ModelSupport.relationInverseMap.get(r);
+            Assertions.assertNotNull(inverse);
+            Assertions.assertNotNull(inverse.getRelType());
+            Assertions.assertNotNull(inverse.getSubReltype());
+            Assertions.assertNotNull(inverse.getRelClass());
+        });
+
+    }
+
+}
