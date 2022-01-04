3bd3653be9e884679c390f8b9f9f36800b657692
Claudio Atzori <claudio.atzori@isti.cnr.it>
2022-01-04 15:02:09 +0100

    OAF-store-graph mdstores: save them in text format

--- dhp-workflows/dhp-aggregation/src/main/java/eu/dnetlib/dhp/collection/CollectionUtils.scala
+++ dhp-workflows/dhp-aggregation/src/main/java/eu/dnetlib/dhp/collection/CollectionUtils.scala
@@ -50,14 +50,16 @@ object CollectionUtils {
 
   def saveDataset(dataset: Dataset[Oaf], targetPath: String): Unit = {
     implicit val resultEncoder: Encoder[Oaf] = Encoders.kryo(classOf[Oaf])
+    val mapper = new ObjectMapper
 
     dataset
       .flatMap(i => CollectionUtils.fixRelations(i))
       .filter(i => i != null)
+      .map(r => mapper.writeValueAsString(r))(Encoders.STRING)
       .write
       .mode(SaveMode.Overwrite)
       .option("compression", "gzip")
-      .json(targetPath)
+      .text(targetPath)
   }
 
 }
