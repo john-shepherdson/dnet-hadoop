1490867cc7886746b5b9925b9bd5a3ebd7499cb5
miriam.baglioni <miriam.baglioni@isti.cnr.it>
2022-02-14 14:52:12 +0100

    [OpenCitation] cleaning of the COCI model

--- dhp-workflows/dhp-aggregation/src/main/java/eu/dnetlib/dhp/actionmanager/opencitations/model/COCI.java
+++ dhp-workflows/dhp-aggregation/src/main/java/eu/dnetlib/dhp/actionmanager/opencitations/model/COCI.java
@@ -6,30 +6,12 @@ import java.io.Serializable;
 import com.opencsv.bean.CsvBindByPosition;
 
 public class COCI implements Serializable {
-	@CsvBindByPosition(position = 0)
-//    @CsvBindByName(column = "doi")
 	private String oci;
 
-	@CsvBindByPosition(position = 1)
-//    @CsvBindByName(column = "level1")
 	private String citing;
 
-	@CsvBindByPosition(position = 2)
-//    @CsvBindByName(column = "level2")
 	private String cited;
 
-	@CsvBindByPosition(position = 3)
-//    @CsvBindByName(column = "level3")
-	private String creation;
-
-	@CsvBindByPosition(position = 4)
-	private String timespan;
-
-	@CsvBindByPosition(position = 5)
-	private String journal_sc;
-
-	@CsvBindByPosition(position = 6)
-	private String author_sc;
 
 	public String getOci() {
 		return oci;
@@ -55,35 +37,5 @@ public class COCI implements Serializable {
 		this.cited = cited;
 	}
 
-	public String getCreation() {
-		return creation;
-	}
-
-	public void setCreation(String creation) {
-		this.creation = creation;
-	}
-
-	public String getTimespan() {
-		return timespan;
-	}
-
-	public void setTimespan(String timespan) {
-		this.timespan = timespan;
-	}
-
-	public String getJournal_sc() {
-		return journal_sc;
-	}
-
-	public void setJournal_sc(String journal_sc) {
-		this.journal_sc = journal_sc;
-	}
-
-	public String getAuthor_sc() {
-		return author_sc;
-	}
-
-	public void setAuthor_sc(String author_sc) {
-		this.author_sc = author_sc;
-	}
+	
 }
