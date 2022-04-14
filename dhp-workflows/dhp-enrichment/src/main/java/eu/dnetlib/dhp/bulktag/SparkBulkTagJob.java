c5a863132c124e25d1b75ca8d7c8546a7db33043
miriam.baglioni <miriam.baglioni@isti.cnr.it>
2022-04-14 14:14:13 +0200

    [BulkTagging] revert it

--- dhp-workflows/dhp-enrichment/src/main/java/eu/dnetlib/dhp/bulktag/SparkBulkTagJob.java
+++ dhp-workflows/dhp-enrichment/src/main/java/eu/dnetlib/dhp/bulktag/SparkBulkTagJob.java
@@ -126,9 +126,6 @@ public class SparkBulkTagJob {
 	// TODO remove this hack as soon as the values fixed by this method will be provided as NON null
 	private static <R extends Result> MapFunction<R, R> patchResult() {
 		return r -> {
-			if(r.getDataInfo() == null){
-				return null;
-			}
 			if (r.getDataInfo().getDeletedbyinference() == null) {
 				r.getDataInfo().setDeletedbyinference(false);
 			}
