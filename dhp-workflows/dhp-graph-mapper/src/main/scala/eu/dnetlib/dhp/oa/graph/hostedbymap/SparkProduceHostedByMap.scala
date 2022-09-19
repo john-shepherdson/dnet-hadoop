ec87149cb3f6f3208210b1d6f72523e6d7a65d49
miriam.baglioni <miriam.baglioni@isti.cnr.it>
2022-09-19 14:06:52 +0200

    [Patch Hosted By] added fix to avoi NPE error when datasource official name is not provided. Removing datasources if no officialname has been provided

--- dhp-workflows/dhp-graph-mapper/src/main/scala/eu/dnetlib/dhp/oa/graph/hostedbymap/SparkProduceHostedByMap.scala
+++ dhp-workflows/dhp-graph-mapper/src/main/scala/eu/dnetlib/dhp/oa/graph/hostedbymap/SparkProduceHostedByMap.scala
@@ -113,27 +113,15 @@ object SparkProduceHostedByMap {
   }
 
   def oaToHostedbyItemType(dats: Datasource): HostedByItemType = {
-    if (dats.getJournal != null) {
-
-      if(dats.getOfficialname != null) {
-        return getHostedByItemType(
-          dats.getId,
-          dats.getOfficialname.getValue,
-          dats.getJournal.getIssnPrinted,
-          dats.getJournal.getIssnOnline,
-          dats.getJournal.getIssnLinking,
-          false
-        )
-      }
+    if (dats.getJournal != null && dats.getOfficialname != null) {
       return getHostedByItemType(
         dats.getId,
-        "",
+        dats.getOfficialname.getValue,
         dats.getJournal.getIssnPrinted,
         dats.getJournal.getIssnOnline,
         dats.getJournal.getIssnLinking,
         false
       )
-
     }
     HostedByItemType("", "", "", "", "", false)
   }
