686580a22068b6437ff4e8dafccbf919d06e2a77
dimitrispie <dpierrakos@gmail.com>
2023-01-12 11:18:03 +0200

    - New Monitor DB workflow - New Organization added

--- dhp-workflows/dhp-monitor-update/src/main/resources/eu/dnetlib/dhp/oa/graph/monitor/oozie_app/monitor.sh
+++ dhp-workflows/dhp-monitor-update/src/main/resources/eu/dnetlib/dhp/oa/graph/monitor/oozie_app/monitor.sh
@@ -8,17 +8,11 @@ fi
 
 export SOURCE=$1
 export TARGET=$2
-export SHADOW=$3
-export SCRIPT_PATH=$4
+export SCRIPT_PATH=$3
 
-export HIVE_OPTS="-hiveconf mapred.job.queue.name=analytics -hiveconf hive.spark.client.connect.timeout=120000ms -hiveconf hive.spark.client.server.connect.timeout=300000ms -hiveconf spark.executor.memory=19166291558 -hiveconf spark.yarn.executor.memoryOverhead=3225 -hiveconf spark.driver.memory=11596411699 -hiveconf spark.yarn.driver.memoryOverhead=1228"
-export HADOOP_USER_NAME="oozie"
-
-echo "Getting file from " $SCRIPT_PATH
-hdfs dfs -copyToLocal $SCRIPT_PATH
+echo "Getting file from " $3
+hdfs dfs -copyToLocal $3
 
 echo "Creating monitor database"
-#cat step20-createMonitorDB.sql | sed s/SOURCE/$1/g | sed s/TARGET/$2/g1 > foo
-cat createMonitorDB.sql | sed "s/TARGET/${TARGET}/g" | sed "s/SOURCE/${SOURCE}/g" > foo
-hive $HIVE_OPTS -f foo
-echo "Hive shell finished"
+cat createMonitorDB.sql | sed s/SOURCE/$1/g | sed s/TARGET/$2/g1 | impala-shell -f -
+echo "Impala shell finished"
