f3aaff36884d68746f0d2b6a05370468c74aa4c0
dimitrispie <dpierrakos@gmail.com>
2023-02-14 09:48:36 +0200

    Remove duplicate orgs

--- dhp-workflows/dhp-stats-update/src/main/resources/eu/dnetlib/dhp/oa/graph/stats/oozie_app/scripts/step20-createMonitorDB.sql
+++ dhp-workflows/dhp-stats-update/src/main/resources/eu/dnetlib/dhp/oa/graph/stats/oozie_app/scripts/step20-createMonitorDB.sql
@@ -63,9 +63,6 @@ create table TARGET.result stored as parquet as
              'openorgs____::b8b8ca674452579f3f593d9f5e557483',   -- University College Cork
              'openorgs____::38d7097854736583dde879d12dacafca'	-- Brown University
              'openorgs____::57784c9e047e826fefdb1ef816120d92', --Arts et Métiers ParisTech
-             'openorgs____::b8b8ca674452579f3f593d9f5e557483',   -- University College Cork
-             'openorgs____::38d7097854736583dde879d12dacafca',	-- Brown University
-             'openorgs____::57784c9e047e826fefdb1ef816120d92',  --Arts et Métiers ParisTech
              'openorgs____::2530baca8a15936ba2e3297f2bce2e7e'	-- University of Cape Town
         ) )) foo;
 
