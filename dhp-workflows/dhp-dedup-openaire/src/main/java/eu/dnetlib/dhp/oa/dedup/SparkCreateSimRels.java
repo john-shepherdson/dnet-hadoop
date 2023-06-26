75ff902f9d96d0e8f6a65fd42ea44c550efb9a45
Claudio Atzori <claudio.atzori@isti.cnr.it>
2023-06-26 16:50:59 +0200

    WIP: various refactors

--- dhp-workflows/dhp-dedup-openaire/src/main/java/eu/dnetlib/dhp/oa/dedup/SparkCreateSimRels.java
+++ dhp-workflows/dhp-dedup-openaire/src/main/java/eu/dnetlib/dhp/oa/dedup/SparkCreateSimRels.java
@@ -81,6 +81,8 @@ public class SparkCreateSimRels extends AbstractSparkAction {
 			final String outputPath = DedupUtility.createSimRelPath(workingPath, actionSetId, subEntity);
 			removeOutputDir(spark, outputPath);
 
+			JavaSparkContext sc = JavaSparkContext.fromSparkContext(spark.sparkContext());
+
 			SparkDedupConfig sparkConfig = new SparkDedupConfig(dedupConf, numPartitions);
 
 			Dataset<?> simRels = spark
