326c9dc08ceac7613c187f3d3c3609731823f8bc
Giambattista Bloisi <gbloisi@gmail.com>
2023-08-02 18:05:53 +0200

    Changes in maven poms to build and test the project using Spark 3.4.x and scala 2.12

--- dhp-workflows/dhp-graph-provision/src/main/java/eu/dnetlib/dhp/oa/provision/utils/TemplateFactory.java
+++ dhp-workflows/dhp-graph-provision/src/main/java/eu/dnetlib/dhp/oa/provision/utils/TemplateFactory.java
@@ -5,13 +5,10 @@ import static eu.dnetlib.dhp.oa.provision.utils.GraphMappingUtils.removePrefix;
 import static eu.dnetlib.dhp.oa.provision.utils.XmlSerializationUtils.escapeXml;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-
-import javax.swing.text.html.Option;
 
 import org.apache.commons.lang3.StringUtils;
 import org.stringtemplate.v4.ST;
