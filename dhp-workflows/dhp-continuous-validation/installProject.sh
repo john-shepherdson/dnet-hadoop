eee085c1046e636597ee1209a6c16c17665976a7
LSmyrnaios <lsmyrnaios@gmail.com>
2024-01-24 14:08:32 +0200

    Force a check for updated releases and snapshots on remote repositories, when building the Continuous-Validation workflow.

--- dhp-workflows/dhp-continuous-validation/installProject.sh
+++ dhp-workflows/dhp-continuous-validation/installProject.sh
@@ -4,7 +4,7 @@ DEFAULT_PROFILE=''  # It's the empty profile.
 NEWER_VERSIONS_PROFILE='-Pscala-2.12'
 CHOSEN_MAVEN_PROFILE=${DEFAULT_PROFILE}
 
-mvn clean install ${CHOSEN_MAVEN_PROFILE} -Dmaven.test.skip=true
+mvn clean install -U ${CHOSEN_MAVEN_PROFILE} -Dmaven.test.skip=true
 
 # We skip tests for all modules, since the take a big amount of time and some of them fail.
 # Any test added to this module, will be executed in the "runOozieWorkflow.sh" script.
--- dhp-workflows/dhp-continuous-validation/runOozieWorkflow.sh
+++ dhp-workflows/dhp-continuous-validation/runOozieWorkflow.sh
@@ -5,8 +5,8 @@ NEWER_VERSIONS_PROFILE='-Pscala-2.12'
 CHOSEN_MAVEN_PROFILE=${DEFAULT_PROFILE}
 
 
-mvn clean package ${CHOSEN_MAVEN_PROFILE} -Poozie-package,deploy,run \
+mvn clean package -U ${CHOSEN_MAVEN_PROFILE} -Poozie-package,deploy,run \
       -Dworkflow.source.dir=eu/dnetlib/dhp/continuous_validator
 
-echo -e "\n\nShowing the contents of \"extract-and-run-on-remote-host.log\":\n\n"
+echo -e "\n\nShowing the contents of \"extract-and-run-on-remote-host.log\":\n"
 cat ./target/extract-and-run-on-remote-host.log
