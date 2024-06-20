9f6e16a03cb5f55f58902e97b05a041ff66abd4b
Serafeim Chatzopoulos <s.chatzopoulos@gmail.com>
2024-06-20 16:03:15 +0300

    Add support to cretate/update solr collection aliases

--- dhp-workflows/dhp-graph-provision/src/main/java/eu/dnetlib/dhp/oa/provision/SolrAdminApplication.java
+++ dhp-workflows/dhp-graph-provision/src/main/java/eu/dnetlib/dhp/oa/provision/SolrAdminApplication.java
@@ -9,6 +9,7 @@ import org.apache.commons.io.IOUtils;
 import org.apache.solr.client.solrj.SolrResponse;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.CloudSolrClient;
+import org.apache.solr.client.solrj.request.CollectionAdminRequest;
 import org.apache.solr.client.solrj.response.UpdateResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,7 +24,7 @@ public class SolrAdminApplication implements Closeable {
 	private static final Logger log = LoggerFactory.getLogger(SolrAdminApplication.class);
 
 	enum Action {
-		DELETE_BY_QUERY, COMMIT
+		DELETE_BY_QUERY, COMMIT, UPDATE_ALIASES
 	}
 
 	private final CloudSolrClient solrClient;
@@ -62,8 +63,21 @@ public class SolrAdminApplication implements Closeable {
 		final String collection = ProvisionConstants.getCollectionName(format);
 		log.info("collection: {}", collection);
 
+		final String publicFormat = parser.get("publicFormat");
+		log.info("publicFormat: {}", publicFormat);
+
+		final String shadowFormat = parser.get("shadowFormat");
+		log.info("shadowFormat: {}", shadowFormat);
+
+		// get collection names from metadata format profiles names
+		final String publicCollection = ProvisionConstants.getCollectionName(publicFormat);
+		log.info("publicCollection: {}", publicCollection);
+
+		final String shadowCollection = ProvisionConstants.getCollectionName(shadowFormat);
+		log.info("shadowCollection: {}", shadowCollection);
+
 		try (SolrAdminApplication app = new SolrAdminApplication(zkHost)) {
-			app.execute(action, collection, query, commit);
+			app.execute(action, collection, query, commit, publicCollection, shadowCollection);
 		}
 	}
 
@@ -73,21 +87,28 @@ public class SolrAdminApplication implements Closeable {
 	}
 
 	public SolrResponse commit(String collection) throws IOException, SolrServerException {
-		return execute(Action.COMMIT, collection, null, true);
+		return execute(Action.COMMIT, collection, null, true, null, null);
 	}
 
-	public SolrResponse execute(Action action, String collection, String query, boolean commit)
+	public SolrResponse execute(Action action, String collection, String query, boolean commit,
+		String publicCollection, String shadowCollection)
 		throws IOException, SolrServerException {
 		switch (action) {
 
 			case DELETE_BY_QUERY:
 				UpdateResponse rsp = solrClient.deleteByQuery(collection, query);
 				if (commit) {
-					solrClient.commit(collection);
+					return solrClient.commit(collection);
 				}
 				return rsp;
+
 			case COMMIT:
 				return solrClient.commit(collection);
+
+			case UPDATE_ALIASES:
+				this.updateAliases(publicCollection, shadowCollection);
+				return null;
+
 			default:
 				throw new IllegalArgumentException("action not managed: " + action);
 		}
@@ -96,6 +117,30 @@ public class SolrAdminApplication implements Closeable {
 	@Override
 	public void close() throws IOException {
 		solrClient.close();
+	}
+
+	private void updateAliases(String publicCollection, String shadowCollection)
+		throws SolrServerException, IOException {
+
+		// delete current aliases
+		this.deleteAlias(ProvisionConstants.PUBLIC_ALIAS_NAME);
+		this.deleteAlias(ProvisionConstants.SHADOW_ALIAS_NAME);
+
+		// create aliases
+		this.createAlias(ProvisionConstants.PUBLIC_ALIAS_NAME, publicCollection);
+		this.createAlias(ProvisionConstants.SHADOW_ALIAS_NAME, shadowCollection);
+
+	}
+
+	public SolrResponse deleteAlias(String aliasName) throws SolrServerException, IOException {
+		CollectionAdminRequest.DeleteAlias deleteAliasRequest = CollectionAdminRequest.deleteAlias(aliasName);
+		return deleteAliasRequest.process(solrClient);
+	}
+
+	public SolrResponse createAlias(String aliasName, String collection) throws IOException, SolrServerException {
+		CollectionAdminRequest.CreateAlias createAliasRequest = CollectionAdminRequest
+			.createAlias(aliasName, collection);
+		return createAliasRequest.process(solrClient);
 	}
 
 }
--- dhp-workflows/dhp-graph-provision/src/test/java/eu/dnetlib/dhp/oa/provision/SolrAdminApplicationTest.java
+++ dhp-workflows/dhp-graph-provision/src/test/java/eu/dnetlib/dhp/oa/provision/SolrAdminApplicationTest.java
@@ -4,9 +4,9 @@ package eu.dnetlib.dhp.oa.provision;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.apache.solr.client.solrj.response.CollectionAdminResponse;
 import org.apache.solr.client.solrj.response.SolrPingResponse;
 import org.apache.solr.client.solrj.response.UpdateResponse;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class SolrAdminApplicationTest extends SolrTest {
@@ -24,7 +24,7 @@ class SolrAdminApplicationTest extends SolrTest {
 		SolrAdminApplication admin = new SolrAdminApplication(miniCluster.getSolrClient().getZkHost());
 
 		UpdateResponse rsp = (UpdateResponse) admin
-			.execute(SolrAdminApplication.Action.DELETE_BY_QUERY, DEFAULT_COLLECTION, "*:*", false);
+			.execute(SolrAdminApplication.Action.DELETE_BY_QUERY, DEFAULT_COLLECTION, "*:*", false, null, null);
 
 		assertEquals(0, rsp.getStatus());
 	}
@@ -37,6 +37,27 @@ class SolrAdminApplicationTest extends SolrTest {
 		UpdateResponse rsp = (UpdateResponse) admin.commit(DEFAULT_COLLECTION);
 
 		assertEquals(0, rsp.getStatus());
+	}
+
+	@Test
+	void testAdminApplication_CREATE_ALIAS() throws Exception {
+
+		SolrAdminApplication admin = new SolrAdminApplication(miniCluster.getSolrClient().getZkHost());
+
+		CollectionAdminResponse rsp = (CollectionAdminResponse) admin
+			.createAlias(ProvisionConstants.PUBLIC_ALIAS_NAME, DEFAULT_COLLECTION);
+		assertEquals(0, rsp.getStatus());
+
+	}
+
+	@Test
+	void testAdminApplication_DELETE_ALIAS() throws Exception {
+
+		SolrAdminApplication admin = new SolrAdminApplication(miniCluster.getSolrClient().getZkHost());
+
+		CollectionAdminResponse rsp = (CollectionAdminResponse) admin.deleteAlias(ProvisionConstants.PUBLIC_ALIAS_NAME);
+		assertEquals(0, rsp.getStatus());
+
 	}
 
 }
