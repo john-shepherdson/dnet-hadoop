b822b34abeda50dacb46f64245c384ba76aa6090
Claudio Atzori <claudio.atzori@isti.cnr.it>
2024-07-01 09:22:35 +0200

    code formatting

--- dhp-pace-core/src/main/java/eu/dnetlib/pace/tree/CountryMatch.java
+++ dhp-pace-core/src/main/java/eu/dnetlib/pace/tree/CountryMatch.java
@@ -1,3 +1,4 @@
+
 package eu.dnetlib.pace.tree;
 
 import java.util.Map;
@@ -11,37 +12,37 @@ import eu.dnetlib.pace.tree.support.ComparatorClass;
 @ComparatorClass("countryMatch")
 public class CountryMatch extends AbstractStringComparator {
 
-    public CountryMatch(Map<String, String> params) {
-        super(params, new com.wcohen.ss.JaroWinkler());
-    }
+	public CountryMatch(Map<String, String> params) {
+		super(params, new com.wcohen.ss.JaroWinkler());
+	}
 
-    public CountryMatch(final double weight) {
-        super(weight, new com.wcohen.ss.JaroWinkler());
-    }
+	public CountryMatch(final double weight) {
+		super(weight, new com.wcohen.ss.JaroWinkler());
+	}
 
-    protected CountryMatch(final double weight, final AbstractStringDistance ssalgo) {
-        super(weight, ssalgo);
-    }
+	protected CountryMatch(final double weight, final AbstractStringDistance ssalgo) {
+		super(weight, ssalgo);
+	}
 
-    @Override
-    public double distance(final String a, final String b, final Config conf) {
-        if (a.isEmpty() || b.isEmpty()) {
-            return -1.0; // return -1 if a field is missing
-        }
-        if (a.equalsIgnoreCase("unknown") || b.equalsIgnoreCase("unknown")) {
-            return -1.0; // return -1 if a country is UNKNOWN
-        }
+	@Override
+	public double distance(final String a, final String b, final Config conf) {
+		if (a.isEmpty() || b.isEmpty()) {
+			return -1.0; // return -1 if a field is missing
+		}
+		if (a.equalsIgnoreCase("unknown") || b.equalsIgnoreCase("unknown")) {
+			return -1.0; // return -1 if a country is UNKNOWN
+		}
 
-        return a.equals(b) ? 1.0 : 0;
-    }
+		return a.equals(b) ? 1.0 : 0;
+	}
 
-    @Override
-    public double getWeight() {
-        return super.weight;
-    }
+	@Override
+	public double getWeight() {
+		return super.weight;
+	}
 
-    @Override
-    protected double normalize(final double d) {
-        return d;
-    }
+	@Override
+	protected double normalize(final double d) {
+		return d;
+	}
 }
