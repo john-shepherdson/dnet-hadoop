6b0f7cc8b09f93ba6435682137d691fd58bfd75e
michele.artini <michele.artini@isti.cnr.it>
2024-09-23 10:16:53 +0200

    skip urls with authentication

--- dhp-workflows/dhp-aggregation/src/test/java/eu/dnetlib/dhp/collection/plugin/osf/OsfPreprintsCollectorPluginTest.java
+++ dhp-workflows/dhp-aggregation/src/test/java/eu/dnetlib/dhp/collection/plugin/osf/OsfPreprintsCollectorPluginTest.java
@@ -1,11 +1,15 @@
 
 package eu.dnetlib.dhp.collection.plugin.osf;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
 import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Stream;
 
+import org.dom4j.DocumentHelper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
@@ -17,6 +21,7 @@ import eu.dnetlib.dhp.collection.ApiDescriptor;
 import eu.dnetlib.dhp.common.aggregation.AggregatorReport;
 import eu.dnetlib.dhp.common.collection.CollectorException;
 import eu.dnetlib.dhp.common.collection.HttpClientParams;
+import eu.dnetlib.dhp.common.collection.HttpConnector2;
 
 public class OsfPreprintsCollectorPluginTest {
 
@@ -50,7 +55,7 @@ public class OsfPreprintsCollectorPluginTest {
 	}
 
 	@Test
-	// @Disabled
+	@Disabled
 	void test_limited() throws CollectorException {
 		final AtomicInteger i = new AtomicInteger(0);
 		final Stream<String> stream = this.plugin.collect(this.api, new AggregatorReport());
@@ -81,6 +86,28 @@ public class OsfPreprintsCollectorPluginTest {
 
 		log.info("TOTAL: {}", i.get());
 		Assertions.assertTrue(i.get() > 0);
+	}
+
+	@Test
+	// @Disabled
+	void test_authentication_required() {
+		final HttpConnector2 connector = new HttpConnector2();
+
+		try {
+			final String res = connector.getInputSource("https://api.osf.io/v2/preprints/ydtzx/contributors/?format=json");
+			System.out.println(res);
+			fail();
+		} catch (final Throwable e) {
+
+			System.out.println("**** ERROR: " + e.getMessage());
+
+			if ((e instanceof CollectorException) && e.getMessage().contains("401")) {
+				System.out.println(" XML: " + DocumentHelper.createDocument().getRootElement().detach());
+			}
+
+			assertTrue(e.getMessage().contains("401"));
+		}
+
 	}
 
 }
