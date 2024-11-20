496007188a720b18ce8301b6050c6e0b924522d1
sandro.labruzzo <sandro.labruzzo@gmail.com>
2024-11-20 09:50:09 +0100

    Added assertion on CrossrefMappingTest

--- dhp-workflows/dhp-aggregation/src/test/scala/eu/dnetlib/dhp/collection/crossref/CrossrefMappingTest.scala
+++ dhp-workflows/dhp-aggregation/src/test/scala/eu/dnetlib/dhp/collection/crossref/CrossrefMappingTest.scala
@@ -3,11 +3,14 @@ package eu.dnetlib.dhp.collection.crossref
 import com.fasterxml.jackson.databind.ObjectMapper
 import eu.dnetlib.dhp.aggregation.AbstractVocabularyTest
 import eu.dnetlib.dhp.collection.crossref.Crossref2Oaf.TransformationType
+import eu.dnetlib.dhp.schema.oaf.Publication
 import org.apache.commons.io.IOUtils
-import org.junit.jupiter.api.{BeforeEach, Test}
+import org.junit.jupiter.api.{Assertions, BeforeEach, Test}
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.junit.jupiter.MockitoExtension
 import org.slf4j.{Logger, LoggerFactory}
+
+import scala.collection.JavaConverters.asScalaBufferConverter
 
 @ExtendWith(Array(classOf[MockitoExtension]))
 class CrossrefMappingTest extends AbstractVocabularyTest {
@@ -26,7 +29,7 @@ class CrossrefMappingTest extends AbstractVocabularyTest {
       IOUtils.toString(getClass.getResourceAsStream("/eu/dnetlib/dhp/collection/crossref/issn_pub.json"), "utf-8")
 
     Crossref2Oaf.convert(input, vocabularies, TransformationType.All).foreach(record => {
-      println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(record))
+      Assertions.assertNotNull(record)
     })
 
   }
@@ -37,6 +40,16 @@ class CrossrefMappingTest extends AbstractVocabularyTest {
     val input =
       IOUtils.toString(getClass.getResourceAsStream("/eu/dnetlib/dhp/collection/crossref/affiliationTest.json"), "utf-8")
     val data = Crossref2Oaf.convert(input, vocabularies, TransformationType.OnlyResult)
+    data.foreach(record => {
+      Assertions.assertNotNull(record)
+      Assertions.assertTrue(record.isInstanceOf[Publication])
+      val publication = record.asInstanceOf[Publication]
+      publication.getAuthor.asScala.foreach(author => {
+        Assertions.assertNotNull(author.getRawAffiliationString)
+        Assertions.assertTrue(author.getRawAffiliationString.size()>0)
+
+        })
+      })
     println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(data.head))
   }
 }
