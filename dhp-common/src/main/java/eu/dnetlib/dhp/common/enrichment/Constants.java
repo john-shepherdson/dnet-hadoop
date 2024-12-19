6813065de2a8808f512cc93a51a12ce1c8b9c553
Miriam Baglioni <miriam.baglioni@isti.cnr.it>
2024-12-19 11:01:45 +0100

    [orcidPropagatio] added classid and classname for qualifier of the pid

--- dhp-common/src/main/java/eu/dnetlib/dhp/common/enrichment/Constants.java
+++ dhp-common/src/main/java/eu/dnetlib/dhp/common/enrichment/Constants.java
@@ -3,4 +3,7 @@ package eu.dnetlib.dhp.common.enrichment;
 
 public class Constants {
 	public static final String PROPAGATION_DATA_INFO_TYPE = "propagation";
+	public static final String PROPAGATION_ORCID_TO_RESULT_FROM_SEM_REL_CLASS_ID = "authorpid:result";
+	public static final String PROPAGATION_ORCID_TO_RESULT_FROM_SEM_REL_CLASS_NAME = "Propagation of authors pid to result through semantic relations";
+
 }
