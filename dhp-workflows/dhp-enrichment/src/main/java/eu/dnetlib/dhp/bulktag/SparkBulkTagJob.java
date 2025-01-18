80e41c9310b8b5f6d96b1cad666d560b619c8649
Miriam Baglioni <miriam.baglioni@isti.cnr.it>
2025-01-18 14:58:22 +0100

    [bulkTag] refactoring

--- dhp-workflows/dhp-enrichment/src/main/java/eu/dnetlib/dhp/bulktag/SparkBulkTagJob.java
+++ dhp-workflows/dhp-enrichment/src/main/java/eu/dnetlib/dhp/bulktag/SparkBulkTagJob.java
@@ -118,11 +118,7 @@ public class SparkBulkTagJob {
 			spark -> {
 				extendCommunityConfigurationForEOSC(spark, inputPath, cc);
 				ReadDatasourceMasterDuplicateFromDB.execute(dbUrl, dbUser, dbPassword, hdfsPath, hdfsNameNode);
-				execEntityTag(
-						spark, inputPath + "datasource", outputPath + "datasource",
-						mapWithMasterDatasource(spark, hdfsPath, Utils.getDatasourceCommunities(baseURL)),
-						Datasource.class, TaggingConstants.CLASS_ID_DATASOURCE,
-						TaggingConstants.CLASS_NAME_BULKTAG_DATASOURCE);
+
 				execBulkTag(
 					spark, inputPath, outputPath, protoMap, cc);
 				execEntityTag(
@@ -135,7 +131,11 @@ public class SparkBulkTagJob {
 					spark, inputPath + "project", outputPath + "project",
 					Utils.getProjectCommunityMap(baseURL),
 					Project.class, TaggingConstants.CLASS_ID_PROJECT, TaggingConstants.CLASS_NAME_BULKTAG_PROJECT);
-
+				execEntityTag(
+						spark, inputPath + "datasource", outputPath + "datasource",
+						mapWithMasterDatasource(spark, hdfsPath, Utils.getDatasourceCommunities(baseURL)),
+						Datasource.class, TaggingConstants.CLASS_ID_DATASOURCE,
+						TaggingConstants.CLASS_NAME_BULKTAG_DATASOURCE);
 
 			});
 	}
@@ -155,7 +155,7 @@ public class SparkBulkTagJob {
 		Dataset<String> datasourceIdentifiers = spark.createDataset(idList, Encoders.STRING());
 		List<Row> mappedKeys = masterDuplicate
 			.join(
-					datasourceIdentifiers, datasourceIdentifiers.col("value").equalTo(masterDuplicate.col("duplicateId")),
+				datasourceIdentifiers, datasourceIdentifiers.col("value").equalTo(masterDuplicate.col("duplicateId")),
 				"left_semi")
 			.selectExpr("masterId as source", "duplicateId as target")
 			.collectAsList();
