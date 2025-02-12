68f5ecc852f5bea91cbb10ed9ee82fe8a7140e13
miconis <michele.debonis@isti.cnr.it>
2025-02-12 14:04:50 +0100

    WIP: relationships redirection mechamism implemented

--- dhp-workflows/dhp-aggregation/src/main/java/eu/dnetlib/dhp/actionmanager/raid/GenerateRAiDActionSetJob.java
+++ dhp-workflows/dhp-aggregation/src/main/java/eu/dnetlib/dhp/actionmanager/raid/GenerateRAiDActionSetJob.java
@@ -15,8 +15,12 @@ import org.apache.commons.io.IOUtils;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.SequenceFileOutputFormat;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -74,13 +78,16 @@ public class GenerateRAiDActionSetJob {
 		log.info("inputPath: {}", inputPath);
 
 		final String outputPath = parser.get("outputPath");
-		log.info("outputPath {}: ", outputPath);
+		log.info("outputPath: {}", outputPath);
+
+		final String graphBasePath = parser.get("graphBasePath");
+		log.info("graphBasePath: {}", graphBasePath);
 
 		final SparkConf conf = new SparkConf();
 
 		runWithSparkSession(conf, isSparkSessionManaged, spark -> {
 			removeOutputDir(spark, outputPath);
-			processRAiDEntities(spark, inputPath, outputPath);
+			processRAiDEntities(spark, inputPath, outputPath, graphBasePath);
 		});
 	}
 
@@ -90,15 +97,154 @@ public class GenerateRAiDActionSetJob {
 
 	static void processRAiDEntities(final SparkSession spark,
 		final String inputPath,
-		final String outputPath) {
-		readInputPath(spark, inputPath)
-			.map(GenerateRAiDActionSetJob::prepareRAiD)
-			.flatMap(List::iterator)
+		final String outputPath,
+		final String graphBasePath) {
+
+		JavaRDD<? extends Oaf> entities = readInputPath(spark, inputPath)
+			.map(GenerateRAiDActionSetJob::prepareGraphEntities)
+			.flatMap(List::iterator);
+
+		// <raid_id, result_id>
+		JavaPairRDD<String, Relation> raidRelations = entities
+			.filter(e -> e instanceof Relation)
+			.map(e -> (Relation) e)
+			.filter(rel -> rel.getRelClass().equals(ModelConstants.HAS_PART))
+			.mapToPair(rel -> new Tuple2<>(rel.getTarget(), rel));
+		// <result_id, project_id/organization_id>
+		JavaPairRDD<String, Relation> relevantRelations = readRelevantRelations(spark, graphBasePath)
+			.mapToPair(rel -> new Tuple2<>(rel.getSource(), rel));
+
+		JavaRDD<Relation> newRelations = relevantRelations
+			.join(raidRelations)
+			.flatMap(x -> {
+				Relation leftRel = x._2()._1();
+				String entityId = leftRel.getTarget();
+				String relType = leftRel.getRelType();
+				Relation raidRel = x._2()._2();
+				String raidId = x._2()._2().getSource();
+
+				List<Relation> res = new ArrayList<>();
+
+				Relation rel1 = getRelation(
+					raidId,
+					entityId,
+					relType,
+					PART,
+					HAS_PART,
+					raidRel.getCollectedfrom(),
+					raidRel.getDataInfo(),
+					raidRel.getLastupdatetimestamp());
+
+				Relation rel2 = getRelation(
+					entityId,
+					raidId,
+					relType,
+					PART,
+					IS_PART_OF,
+					raidRel.getCollectedfrom(),
+					raidRel.getDataInfo(),
+					raidRel.getLastupdatetimestamp());
+
+				res.add(rel1);
+				res.add(rel2);
+				return res.iterator();
+			})
+			.distinct();
+
+		// create actions for new relations
+		JavaRDD<AtomicAction<? extends Oaf>> newActions = newRelations
+			.map(rel -> new AtomicAction<>(Relation.class, rel));
+
+		// add new relations to old and create actions
+		JavaRDD<AtomicAction<? extends Oaf>> actions = entities
+			.map(
+				e -> (e instanceof Relation) ? new AtomicAction<>(Relation.class, (Relation) e)
+					: new AtomicAction<>(OtherResearchProduct.class, (OtherResearchProduct) e))
+			.union(newActions);
+
+		// save result in the action set
+		actions
 			.mapToPair(
 				aa -> new Tuple2<>(new Text(aa.getClazz().getCanonicalName()),
 					new Text(OBJECT_MAPPER.writeValueAsString(aa))))
 			.saveAsHadoopFile(outputPath, Text.class, Text.class, SequenceFileOutputFormat.class);
 
+	}
+
+	protected static List<Oaf> prepareGraphEntities(final RAiDEntity r) {
+
+		final Date now = new Date();
+		final OtherResearchProduct orp = new OtherResearchProduct();
+		final List<Oaf> res = new ArrayList<>();
+		String raidId = calculateOpenaireId(r.getRaid());
+
+		orp.setId(raidId);
+		orp.setCollectedfrom(RAID_COLLECTED_FROM);
+		orp.setDataInfo(RAID_DATA_INFO);
+		orp
+			.setTitle(
+				Collections
+					.singletonList(
+						structuredProperty(
+							r.getTitle(),
+							qualifier("main title", "main title", DNET_DATACITE_TITLE, DNET_DATACITE_TITLE),
+							RAID_DATA_INFO)));
+		orp.setDescription(listFields(RAID_DATA_INFO, r.getSummary()));
+
+		Instance instance = new Instance();
+		instance.setInstancetype(RAID_QUALIFIER);
+		orp.setInstance(Collections.singletonList(instance));
+		orp
+			.setSubject(
+				r
+					.getSubjects()
+					.stream()
+					.map(
+						s -> subject(
+							s,
+							qualifier(
+								DNET_SUBJECT_KEYWORD, DNET_SUBJECT_KEYWORD, DNET_SUBJECT_TYPOLOGIES,
+								DNET_SUBJECT_TYPOLOGIES),
+							RAID_DATA_INFO))
+					.collect(Collectors.toList()));
+		orp
+			.setRelevantdate(
+				Arrays
+					.asList(
+						structuredProperty(
+							r.getEndDate(), qualifier(END_DATE, END_DATE, DNET_DATACITE_DATE, DNET_DATACITE_DATE),
+							RAID_DATA_INFO),
+						structuredProperty(
+							r.getStartDate(),
+							qualifier(START_DATE, START_DATE, DNET_DATACITE_DATE, DNET_DATACITE_DATE),
+							RAID_DATA_INFO)));
+		orp.setLastupdatetimestamp(now.getTime());
+		orp.setDateofacceptance(field(r.getStartDate(), RAID_DATA_INFO));
+
+		res.add(orp);
+
+		for (String resultId : r.getIds()) {
+			Relation rel1 = OafMapperUtils
+				.getRelation(
+					raidId,
+					resultId,
+					ModelConstants.RESULT_RESULT,
+					PART,
+					HAS_PART,
+					orp);
+			Relation rel2 = OafMapperUtils
+				.getRelation(
+					resultId,
+					raidId,
+					ModelConstants.RESULT_RESULT,
+					PART,
+					IS_PART_OF,
+					orp);
+			res.add(rel1);
+			res.add(rel2);
+		}
+
+		return res;
 	}
 
 	protected static List<AtomicAction<? extends Oaf>> prepareRAiD(final RAiDEntity r) {
@@ -198,6 +344,24 @@ public class GenerateRAiDActionSetJob {
 			.json(path)
 			.as(Encoders.bean(RAiDEntity.class))
 			.toJavaRDD();
+	}
+
+	private static JavaRDD<Relation> readRelevantRelations(
+		final SparkSession spark,
+		final String graphBasePath) {
+		// keep only relations between research products and projects/organization
+		return spark
+				.read()
+				.schema(Encoders.bean(Relation.class).schema())
+				.json(graphBasePath + "/relation")
+				.as(Encoders.bean(Relation.class))
+				.toJavaRDD()
+				.filter(
+					rel -> rel.getRelType().equals(RESULT_PROJECT)
+						|| rel.getRelType().equals(RESULT_ORGANIZATION))
+				.filter(
+					rel -> rel.getRelClass().equals(IS_PRODUCED_BY)
+						|| rel.getRelClass().equals(HAS_AUTHOR_INSTITUTION));
 	}
 
 }
--- dhp-workflows/dhp-aggregation/src/test/java/eu/dnetlib/dhp/actionmanager/raid/GenerateRAiDActionSetJobTest.java
+++ dhp-workflows/dhp-aggregation/src/test/java/eu/dnetlib/dhp/actionmanager/raid/GenerateRAiDActionSetJobTest.java
@@ -37,6 +37,9 @@ import scala.Tuple2;
 public class GenerateRAiDActionSetJobTest {
 	private static String input_path;
 	private static String output_path;
+
+	private static String graph_base_path;
+
 	static SparkSession spark;
 
 	@BeforeEach
@@ -46,6 +49,14 @@ public class GenerateRAiDActionSetJobTest {
 			.get(
 				GenerateRAiDActionSetJobTest.class
 					.getResource("/eu/dnetlib/dhp/actionmanager/raid/raid_example.json")
+					.toURI())
+			.toFile()
+			.getAbsolutePath();
+
+		graph_base_path = Paths
+			.get(
+				GenerateRAiDActionSetJobTest.class
+					.getResource("/eu/dnetlib/dhp/actionmanager/raid/")
 					.toURI())
 			.toFile()
 			.getAbsolutePath();
@@ -79,7 +90,8 @@ public class GenerateRAiDActionSetJobTest {
 	@Test
 	@Disabled
 	void testProcessRAiDEntities() {
-		GenerateRAiDActionSetJob.processRAiDEntities(spark, input_path, output_path + "/test_raid_action_set");
+		GenerateRAiDActionSetJob
+			.processRAiDEntities(spark, input_path, output_path + "/test_raid_action_set", graph_base_path);
 
 		JavaSparkContext sc = JavaSparkContext.fromSparkContext(spark.sparkContext());
 
@@ -88,7 +100,7 @@ public class GenerateRAiDActionSetJobTest {
 			.map(value -> OBJECT_MAPPER.readValue(value._2().toString(), AtomicAction.class))
 			.map(AtomicAction::getPayload);
 
-		assertEquals(80, result.count());
+		assertEquals(86, result.count());
 	}
 
 	@Test
@@ -162,4 +174,74 @@ public class GenerateRAiDActionSetJobTest {
 
 	}
 
+	@Test
+	void testPrepareGraphRAiD() {
+
+		List<Oaf> entities = GenerateRAiDActionSetJob
+				.prepareGraphEntities(
+						new RAiDEntity(
+								"-92190526",
+								Arrays
+										.asList(
+												"Berli, Justin", "Le Mao, Bérénice", "Guillaume Touya", "Wenclik, Laura",
+												"Courtial, Azelle", "Muehlenhaus, Ian", "Justin Berli", "Touya, Guillaume",
+												"Gruget, Maïeul", "Azelle Courtial", "Ian Muhlenhaus", "Maïeul Gruget", "Marion Dumont",
+												"Maïeul GRUGET", "Cécile Duchêne"),
+								"2021-09-10",
+								"2024-02-16",
+								Arrays
+										.asList(
+												"cartography, zoom, pan, desert fog", "Road network", "zooming", "Pan-scalar maps",
+												"pan-scalar map", "Python library", "QGIS", "map design", "landmarks",
+												"Cartes transscalaires", "anchor", "disorientation", "[INFO]Computer Science [cs]",
+												"[SHS.GEO]Humanities and Social Sciences/Geography", "cognitive cartography",
+												"eye-tracking", "Computers in Earth Sciences", "Topographic map", "National Mapping Agency",
+												"General Medicine", "Geography, Planning and Development", "multi-scales",
+												"pan-scalar maps", "Selection", "cartography", "General Earth and Planetary Sciences",
+												"progressiveness", "map generalisation", "Eye-tracker", "zoom", "algorithms", "Map Design",
+												"cartography, map generalisation, zoom, multi-scale map", "Interactive maps",
+												"Map generalisation", "Earth and Planetary Sciences (miscellaneous)",
+												"Cartographic generalization", "rivers", "Benchmark", "General Environmental Science",
+												"open source", "drawing", "Constraint", "Multi-scale maps"),
+								Arrays
+										.asList(
+												"Where do people look at during multi-scale map tasks?", "FogDetector survey raw data",
+												"Collection of cartographic disorientation stories", "Anchorwhat dataset",
+												"BasqueRoads: A Benchmark for Road Network Selection",
+												"Progressive river network selection for pan-scalar maps",
+												"BasqueRoads, a dataset to benchmark road selection algorithms",
+												"Missing the city for buildings? A critical review of pan-scalar map generalization and design in contemporary zoomable maps",
+												"Empirical approach to advance the generalisation of multi-scale maps",
+												"L'Alpe d'Huez: a dataset to benchmark topographic map generalisation",
+												"eye-tracking data from a survey on zooming in a pan-scalar map",
+												"Material of the experiment 'More is Less' from the MapMuxing project",
+												"Cartagen4py, an open source Python library for map generalisation",
+												"L’Alpe d’Huez: A Benchmark for Topographic Map Generalisation"),
+								Arrays
+										.asList(
+												"50|doi_dedup___::6915135e0aa39f913394513f809ae58a",
+												"50|doi_dedup___::754e3c283639bc6e104c925ff3e34007",
+												"50|doi_dedup___::13517477f3c1261d57a3364363ce6ce0",
+												"50|doi_dedup___::675b16c73accc4e7242bbb4ed9b3724a",
+												"50|doi_dedup___::94ce09906b2d7d37eb2206cea8a50153",
+												"50|dedup_wf_002::cc575d5ca5651ff8c3029a3a76e7e70a",
+												"50|doi_dedup___::c5e52baddda17c755d1bae012a97dc13",
+												"50|doi_dedup___::4f5f38c9e08fe995f7278963183f8ad4",
+												"50|doi_dedup___::a9bc4453273b2d02648a5cb453195042",
+												"50|doi_dedup___::5e893dc0cb7624a33f41c9b428bd59f7",
+												"50|doi_dedup___::c1ecdef48fd9be811a291deed950e1c5",
+												"50|doi_dedup___::9e93c8f2d97c35de8a6a57a5b53ef283",
+												"50|dedup_wf_002::d08be0ed27b13d8a880e891e08d093ea",
+												"50|doi_dedup___::f8d8b3b9eddeca2fc0e3bc9e63996555"),
+								"Exploring Multi-Scale Map Generalization and Design",
+								"This project aims to advance the generalization of multi-scale maps by investigating the impact of different design elements on user experience. The research involves collecting and analyzing data from various sources, including surveys, eye-tracking studies, and user experiments. The goal is to identify best practices for map generalization and design, with a focus on reducing disorientation and improving information retrieval during exploration. The project has led to the development of several datasets, including BasqueRoads, AnchorWhat, and L'Alpe d'Huez, which can be used to benchmark road selection algorithms and topographic map generalization techniques. The research has also resulted in the creation of a Python library, Cartagen4py, for map generalization. The findings of this project have the potential to improve the design and usability of multi-scale maps, making them more effective tools for navigation and information retrieval."));
+
+		OtherResearchProduct orp = (OtherResearchProduct) entities.get(0);
+		Relation rel = (Relation) entities.get(1);
+
+		assertEquals("Exploring Multi-Scale Map Generalization and Design", orp.getTitle().get(0).getValue());
+		assertEquals("50|raid________::759a564ce5cc7360cab030c517c7366b", rel.getSource());
+		assertEquals("50|doi_dedup___::6915135e0aa39f913394513f809ae58a", rel.getTarget());
+
+	}
 }
