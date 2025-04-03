bd2b8592074b0930442b0ff3489cd0c75354e27e
Antonis Lempesis <antleb@di.uoa.gr>
2025-04-03 14:34:31 +0300

    avoiding NPEs by checking for null entity_ids

--- dhp-workflows/dhp-usagestats-update/src/main/resources/eu/dnetlib/dhp/oa/graph/usagestats/oozie_app/scripts/Step2-ProcessPiwikLogs.sql
+++ dhp-workflows/dhp-usagestats-update/src/main/resources/eu/dnetlib/dhp/oa/graph/usagestats/oozie_app/scripts/Step2-ProcessPiwikLogs.sql
@@ -12,7 +12,7 @@ SELECT entity_id,
        CONCAT(YEAR(timestamp), '/', LPAD(MONTH(timestamp), 2, '0')) AS month,
        source
 FROM ${usagestats_raw_db}.piwiklog
-WHERE action='action' and (source_item_type='oaItem' or source_item_type='repItem')
+WHERE action='action' and (source_item_type='oaItem' or source_item_type='repItem') AND entity_id IS NOT NULL
 GROUP BY entity_id, CONCAT(YEAR(timestamp), '/', LPAD(MONTH(timestamp), 2, '0')),
 source ORDER BY source, entity_id; /*EOS*/
 
@@ -44,7 +44,7 @@ SELECT entity_id,
        CONCAT(YEAR(timestamp), '/', LPAD(MONTH(timestamp), 2, '0')) AS month,
        source
 FROM ${usagestats_raw_db}.piwiklog
-WHERE action='download' AND (source_item_type='oaItem' OR source_item_type='repItem')
+WHERE action='download' AND (source_item_type='oaItem' OR source_item_type='repItem') AND entity_id IS NOT NULL
 GROUP BY entity_id, CONCAT(YEAR(timestamp), '/', LPAD(MONTH(timestamp), 2, '0')) , source
 ORDER BY source, entity_id, month; /*EOS*/
 
@@ -90,7 +90,7 @@ SELECT entity_id,
        CONCAT(YEAR(timestamp), '/', LPAD(MONTH(timestamp), 2, '0')) AS month,
        source
 FROM ${usagestats_raw_db}.piwiklog
-WHERE action='action' and (source_item_type='oaItem' or source_item_type='repItem') and source=252
+WHERE action='action' and (source_item_type='oaItem' or source_item_type='repItem') and source=252 AND entity_id IS NOT NULL
 GROUP BY entity_id, CONCAT(YEAR(timestamp), '/', LPAD(MONTH(timestamp), 2, '0')), source
 ORDER BY source, entity_id; /*EOS*/
 
@@ -109,7 +109,7 @@ SELECT entity_id,
        CONCAT(YEAR(timestamp), '/', LPAD(MONTH(timestamp), 2, '0')) AS month,
        source
 FROM ${usagestats_raw_db}.piwiklog
-WHERE action='download' and (source_item_type='oaItem' or source_item_type='repItem') and source=252
+WHERE action='download' and (source_item_type='oaItem' or source_item_type='repItem') and source=252 AND entity_id IS NOT NULL
 GROUP BY entity_id, CONCAT(YEAR(timestamp), '/', LPAD(MONTH(timestamp), 2, '0')), source
 ORDER BY source, entity_id; /*EOS*/
 
