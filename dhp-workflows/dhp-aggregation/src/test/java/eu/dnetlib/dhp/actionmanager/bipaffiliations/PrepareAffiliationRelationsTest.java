1873c5e229a0b987ba8581aff6a8d59b260c5bba
Miriam Baglioni <miriam.baglioni@isti.cnr.it>
2025-04-03 11:36:39 +0200

    [affro_affiliations] -

--- dhp-workflows/dhp-aggregation/src/test/java/eu/dnetlib/dhp/actionmanager/bipaffiliations/PrepareAffiliationRelationsTest.java
+++ dhp-workflows/dhp-aggregation/src/test/java/eu/dnetlib/dhp/actionmanager/bipaffiliations/PrepareAffiliationRelationsTest.java
@@ -188,47 +188,47 @@ public class PrepareAffiliationRelationsTest {
 	void testMatchNew() throws Exception {
 
 		String crossrefAffiliationRelationPath = getClass()
-				.getResource("/eu/dnetlib/dhp/actionmanager/bipaffiliations/newTest/crossref")
-				.getPath();
+			.getResource("/eu/dnetlib/dhp/actionmanager/bipaffiliations/newTest/crossref")
+			.getPath();
 
 		String dataciteAffiliationRelationPath = getClass()
-				.getResource("/eu/dnetlib/dhp/actionmanager/bipaffiliations/newTest/datacite")
-				.getPath();
+			.getResource("/eu/dnetlib/dhp/actionmanager/bipaffiliations/newTest/datacite")
+			.getPath();
 
 		String publisherAffiliationRelationPath = getClass()
-				.getResource("/eu/dnetlib/dhp/actionmanager/bipaffiliations/newTest/publishers")
-				.getPath();
+			.getResource("/eu/dnetlib/dhp/actionmanager/bipaffiliations/newTest/publishers")
+			.getPath();
 
 		String pubmedAffiliationRelationPath = getClass()
-				.getResource("/eu/dnetlib/dhp/actionmanager/bipaffiliations/newTest/pubmed")
-				.getPath();
+			.getResource("/eu/dnetlib/dhp/actionmanager/bipaffiliations/newTest/pubmed")
+			.getPath();
 		String webcrawlAffiliationRelationPath = getClass()
-				.getResource("/eu/dnetlib/dhp/actionmanager/bipaffiliations/newTest/webCrawl_IE")
-				.getPath();
+			.getResource("/eu/dnetlib/dhp/actionmanager/bipaffiliations/newTest/webCrawl_IE")
+			.getPath();
 		String openapsAffiliationRelationPath = getClass()
-				.getResource("/eu/dnetlib/dhp/actionmanager/bipaffiliations/newTest/openapc.json")
-				.getPath();
+			.getResource("/eu/dnetlib/dhp/actionmanager/bipaffiliations/newTest/openapc.json")
+			.getPath();
 		String outputPath = workingDir.toString() + "/actionSet";
 
 		PrepareAffiliationRelations
-				.main(
-						new String[] {
-								"-isSparkSessionManaged", Boolean.FALSE.toString(),
-								"-crossrefInputPath", crossrefAffiliationRelationPath,
-								"-pubmedInputPath", pubmedAffiliationRelationPath,
-								"-openapcInputPath", openapsAffiliationRelationPath,
-								"-dataciteInputPath", dataciteAffiliationRelationPath,
-								"-webCrawlInputPath", webcrawlAffiliationRelationPath,
-								"-publisherInputPath", publisherAffiliationRelationPath,
-								"-outputPath", outputPath
-						});
+			.main(
+				new String[] {
+					"-isSparkSessionManaged", Boolean.FALSE.toString(),
+					"-crossrefInputPath", crossrefAffiliationRelationPath,
+					"-pubmedInputPath", pubmedAffiliationRelationPath,
+					"-openapcInputPath", openapsAffiliationRelationPath,
+					"-dataciteInputPath", dataciteAffiliationRelationPath,
+					"-webCrawlInputPath", webcrawlAffiliationRelationPath,
+					"-publisherInputPath", publisherAffiliationRelationPath,
+					"-outputPath", outputPath
+				});
 
 		final JavaSparkContext sc = new JavaSparkContext(spark.sparkContext());
 
 		JavaRDD<Relation> tmp = sc
-				.sequenceFile(outputPath, Text.class, Text.class)
-				.map(value -> OBJECT_MAPPER.readValue(value._2().toString(), AtomicAction.class))
-				.map(aa -> ((Relation) aa.getPayload()));
+			.sequenceFile(outputPath, Text.class, Text.class)
+			.map(value -> OBJECT_MAPPER.readValue(value._2().toString(), AtomicAction.class))
+			.map(aa -> ((Relation) aa.getPayload()));
 
 		// count the number of relations
 		assertEquals(70, tmp.count());// 18 + 24 + 30 * 4 =
@@ -237,24 +237,24 @@ public class PrepareAffiliationRelationsTest {
 		dataset.createOrReplaceTempView("result");
 
 		Dataset<Row> execVerification = spark
-				.sql("select r.relType, r.relClass, r.source, r.target, r.dataInfo.trust from result r");
+			.sql("select r.relType, r.relClass, r.source, r.target, r.dataInfo.trust from result r");
 
 		// verify that we have equal number of bi-directional relations
 		Assertions
-				.assertEquals(
-						35, execVerification
-								.filter(
-										"relClass='" + ModelConstants.HAS_AUTHOR_INSTITUTION + "'")
-								.collectAsList()
-								.size());
+			.assertEquals(
+				35, execVerification
+					.filter(
+						"relClass='" + ModelConstants.HAS_AUTHOR_INSTITUTION + "'")
+					.collectAsList()
+					.size());
 
 		Assertions
-				.assertEquals(
-						35, execVerification
-								.filter(
-										"relClass='" + ModelConstants.IS_AUTHOR_INSTITUTION_OF + "'")
-								.collectAsList()
-								.size());
+			.assertEquals(
+				35, execVerification
+					.filter(
+						"relClass='" + ModelConstants.IS_AUTHOR_INSTITUTION_OF + "'")
+					.collectAsList()
+					.size());
 
 //		// check confidence value of a specific relation
 //		String sourceDOI = "10.1089/10872910260066679";
