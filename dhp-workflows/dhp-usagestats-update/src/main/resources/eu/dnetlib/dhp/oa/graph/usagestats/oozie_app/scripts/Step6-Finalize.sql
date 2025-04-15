efa8d2151aeda94c97ac193ea77d782c7b87116b
Antonis Lempesis <antleb@di.uoa.gr>
2025-04-15 16:30:00 +0300

    fixed parameter name

--- dhp-workflows/dhp-usagestats-update/src/main/resources/eu/dnetlib/dhp/oa/graph/usagestats/oozie_app/scripts/Step6-Finalize.sql
+++ dhp-workflows/dhp-usagestats-update/src/main/resources/eu/dnetlib/dhp/oa/graph/usagestats/oozie_app/scripts/Step6-Finalize.sql
@@ -149,29 +149,29 @@ WHERE s.source = 'IRUS-UK'; /*EOS*/
 CREATE DATABASE IF NOT EXISTS ${permanent_usagestats_db}; /*EOS*/
 
 -- views_stats
-CREATE OR REPLACE VIEW ${openaire_prod_usage_stats}.views_stats AS
+CREATE OR REPLACE VIEW ${permanent_usagestats_db}.views_stats AS
 SELECT * FROM ${usagestats_db}.views_stats; /*EOS*/
 
 -- pageviews_stats
-CREATE OR REPLACE VIEW ${openaire_prod_usage_stats}.pageviews_stats AS
+CREATE OR REPLACE VIEW ${permanent_usagestats_db}.pageviews_stats AS
 SELECT * FROM ${usagestats_db}.pageviews_stats; /*EOS*/
 
 -- downloads_stats
-CREATE OR REPLACE VIEW ${openaire_prod_usage_stats}.downloads_stats AS
+CREATE OR REPLACE VIEW ${permanent_usagestats_db}.downloads_stats AS
 SELECT * FROM ${usagestats_db}.downloads_stats; /*EOS*/
 
 -- usage_stats
-CREATE OR REPLACE VIEW ${openaire_prod_usage_stats}.usage_stats AS
+CREATE OR REPLACE VIEW ${permanent_usagestats_db}.usage_stats AS
 SELECT * FROM ${usagestats_db}.usage_stats; /*EOS*/
 
 -- project_stats
-CREATE OR REPLACE VIEW ${openaire_prod_usage_stats}.project_stats AS
+CREATE OR REPLACE VIEW ${permanent_usagestats_db}.project_stats AS
 SELECT * FROM ${usagestats_db}.project_stats; /*EOS*/
 
 -- datasource_stats
-CREATE OR REPLACE VIEW ${openaire_prod_usage_stats}.datasource_stats AS
+CREATE OR REPLACE VIEW ${permanent_usagestats_db}.datasource_stats AS
 SELECT * FROM ${usagestats_db}.datasource_stats; /*EOS*/
 
 -- COUNTER R5 Metrics
-CREATE OR REPLACE VIEW ${openaire_prod_usage_stats}.counter_r5_stats_with_metrics AS
+CREATE OR REPLACE VIEW ${permanent_usagestats_db}.counter_r5_stats_with_metrics AS
 SELECT * FROM ${usagestats_db}.counter_r5_stats_with_metrics; /*EOS*/
