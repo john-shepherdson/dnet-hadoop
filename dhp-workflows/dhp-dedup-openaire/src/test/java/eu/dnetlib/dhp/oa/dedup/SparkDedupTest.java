9a27c2c1f76707e49bc75b0cbc2e9f0f5ea8977d
Giambattista Bloisi <giambattista.bloisi@openaire.eu>
2025-04-16 22:37:19 +0200

    Fix 10504: blacklists are not matched because of an extra trailing space in concat fields

--- dhp-workflows/dhp-dedup-openaire/src/test/java/eu/dnetlib/dhp/oa/dedup/SparkDedupTest.java
+++ dhp-workflows/dhp-dedup-openaire/src/test/java/eu/dnetlib/dhp/oa/dedup/SparkDedupTest.java
@@ -100,6 +100,7 @@ public class SparkDedupTest implements Serializable {
 		final SparkConf conf = new SparkConf();
 		conf.set("spark.sql.shuffle.partitions", "200");
 		conf.set("spark.sql.warehouse.dir", testOutputBasePath + "/spark-warehouse");
+		conf.set("spark.driver.host", "127.0.0.1");
 		spark = SparkSession
 			.builder()
 			.appName(SparkDedupTest.class.getSimpleName())
