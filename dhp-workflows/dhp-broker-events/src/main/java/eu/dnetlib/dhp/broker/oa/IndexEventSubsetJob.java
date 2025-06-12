a281e8216e2d1b0f88a14a7de311a0dfb651828f
michele.artini <michele.artini@isti.cnr.it>
2025-06-12 08:07:22 +0200

    refactoring of es client

--- dhp-workflows/dhp-broker-events/src/main/java/eu/dnetlib/dhp/broker/oa/IndexEventSubsetJob.java
+++ dhp-workflows/dhp-broker-events/src/main/java/eu/dnetlib/dhp/broker/oa/IndexEventSubsetJob.java
@@ -3,8 +3,6 @@ package eu.dnetlib.dhp.broker.oa;
 
 import java.io.IOException;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.math.NumberUtils;
@@ -13,24 +11,21 @@ import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.MapFunction;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.TypedColumn;
-import org.apache.spark.util.LongAccumulator;
-import org.elasticsearch.spark.rdd.api.java.JavaEsSpark;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import eu.dnetlib.dhp.application.ArgumentApplicationParser;
 import eu.dnetlib.dhp.broker.model.Event;
 import eu.dnetlib.dhp.broker.oa.util.ClusterUtils;
+import eu.dnetlib.dhp.broker.oa.util.ESIndexer;
 import eu.dnetlib.dhp.broker.oa.util.EventGroup;
 import eu.dnetlib.dhp.broker.oa.util.aggregators.subset.EventSubsetAggregator;
 import scala.Tuple2;
@@ -42,10 +37,9 @@ public class IndexEventSubsetJob {
 	public static void main(final String[] args) throws Exception {
 
 		final ArgumentApplicationParser parser = new ArgumentApplicationParser(
-			IOUtils
-				.toString(
-					IndexEventSubsetJob.class
-						.getResourceAsStream("/eu/dnetlib/dhp/broker/oa/index_event_subset.json")));
+				IOUtils
+						.toString(IndexEventSubsetJob.class
+								.getResourceAsStream("/eu/dnetlib/dhp/broker/oa/index_event_subset.json")));
 		parser.parseArgument(args);
 
 		final SparkConf conf = new SparkConf();
@@ -81,36 +75,19 @@ public class IndexEventSubsetJob {
 
 		final TypedColumn<Event, EventGroup> aggr = new EventSubsetAggregator(maxEventsForTopic).toColumn();
 
-		final LongAccumulator total = spark.sparkContext().longAccumulator("total_indexed");
-
 		final long now = new Date().getTime();
 
 		final Dataset<Event> subset = ClusterUtils
-			.readPath(spark, eventsPath, Event.class)
-			.groupByKey(
-				(MapFunction<Event, String>) e -> e.getTopic() + '@' + e.getMap().getTargetDatasourceId(),
-				Encoders.STRING())
-			.agg(aggr)
-			.map((MapFunction<Tuple2<String, EventGroup>, EventGroup>) t -> t._2, Encoders.bean(EventGroup.class))
-			.flatMap((FlatMapFunction<EventGroup, Event>) g -> g.getData().iterator(), Encoders.bean(Event.class));
+				.readPath(spark, eventsPath, Event.class)
+				.groupByKey((MapFunction<Event, String>) e -> e.getTopic() + '@' + e.getMap().getTargetDatasourceId(), Encoders.STRING())
+				.agg(aggr)
+				.map((MapFunction<Tuple2<String, EventGroup>, EventGroup>) t -> t._2, Encoders.bean(EventGroup.class))
+				.flatMap((FlatMapFunction<EventGroup, Event>) g -> g.getData().iterator(), Encoders.bean(Event.class))
+				.map((MapFunction<Event, Event>) e -> prepareEventForIndexing(e, now), Encoders.bean(Event.class));
 
-		final JavaRDD<String> inputRdd = subset
-			.map((MapFunction<Event, String>) e -> prepareEventForIndexing(e, now, total), Encoders.STRING())
-			.javaRDD();
+		final ESIndexer indexer = new ESIndexer(indexHost, index, esBatchWriteRetryCount, esBatchWriteRetryWait, esBatchSizeEntries, esNodesWanOnly);
 
-		final Map<String, String> esCfg = new HashMap<>();
-
-		esCfg.put("es.index.auto.create", "false");
-		esCfg.put("es.nodes", indexHost);
-		esCfg.put("es.mapping.id", "eventId"); // THE PRIMARY KEY
-		esCfg.put("es.batch.write.retry.count", esBatchWriteRetryCount);
-		esCfg.put("es.batch.write.retry.wait", esBatchWriteRetryWait);
-		esCfg.put("es.batch.size.entries", esBatchSizeEntries);
-		esCfg.put("es.nodes.wan.only", esNodesWanOnly);
-
-		log.info("*** Start indexing");
-		JavaEsSpark.saveJsonToEs(inputRdd, index, esCfg);
-		log.info("*** End indexing");
+		indexer.performIndex(subset, "eventId");
 
 		log.info("*** Deleting old events");
 		final String message = deleteOldEvents(brokerApiBaseUrl, now - 1000);
@@ -130,14 +107,13 @@ public class IndexEventSubsetJob {
 
 	}
 
-	private static String prepareEventForIndexing(final Event e, final long creationDate, final LongAccumulator acc)
-		throws JsonProcessingException {
-		acc.add(1);
+	private static Event prepareEventForIndexing(final Event e, final long creationDate)
+			throws JsonProcessingException {
 
 		e.setCreationDate(creationDate);
 		e.setExpiryDate(Long.MAX_VALUE);
 
-		return new ObjectMapper().writeValueAsString(e);
+		return e;
 	}
 
 }
--- dhp-workflows/dhp-broker-events/src/main/java/eu/dnetlib/dhp/broker/oa/IndexNotificationsJob.java
+++ dhp-workflows/dhp-broker-events/src/main/java/eu/dnetlib/dhp/broker/oa/IndexNotificationsJob.java
@@ -2,8 +2,6 @@
 package eu.dnetlib.dhp.broker.oa;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -12,21 +10,15 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.function.MapFunction;
-import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.util.LongAccumulator;
-import org.elasticsearch.spark.rdd.api.java.JavaEsSpark;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import eu.dnetlib.dhp.application.ArgumentApplicationParser;
 import eu.dnetlib.dhp.broker.model.Notification;
 import eu.dnetlib.dhp.broker.oa.util.ClusterUtils;
+import eu.dnetlib.dhp.broker.oa.util.ESIndexer;
 
 public class IndexNotificationsJob {
 
@@ -35,10 +27,9 @@ public class IndexNotificationsJob {
 	public static void main(final String[] args) throws Exception {
 
 		final ArgumentApplicationParser parser = new ArgumentApplicationParser(
-			IOUtils
-				.toString(
-					IndexNotificationsJob.class
-						.getResourceAsStream("/eu/dnetlib/dhp/broker/oa/index_notifications.json")));
+				IOUtils
+						.toString(IndexNotificationsJob.class
+								.getResourceAsStream("/eu/dnetlib/dhp/broker/oa/index_notifications.json")));
 		parser.parseArgument(args);
 
 		final SparkConf conf = new SparkConf();
@@ -69,31 +60,17 @@ public class IndexNotificationsJob {
 
 		final SparkSession spark = SparkSession.builder().config(conf).getOrCreate();
 
-		final LongAccumulator total = spark.sparkContext().longAccumulator("total_indexed");
-
 		final Long date = ClusterUtils
-			.readPath(spark, notificationsPath, Notification.class)
-			.first()
-			.getDate();
+				.readPath(spark, notificationsPath, Notification.class)
+				.first()
+				.getDate();
 
-		final JavaRDD<String> toIndexRdd = ClusterUtils
-			.readPath(spark, notificationsPath, Notification.class)
-			.map((MapFunction<Notification, String>) n -> prepareForIndexing(n, total), Encoders.STRING())
-			.javaRDD();
+		final Dataset<Notification> dataset = ClusterUtils
+				.readPath(spark, notificationsPath, Notification.class);
 
-		final Map<String, String> esCfg = new HashMap<>();
+		final ESIndexer indexer = new ESIndexer(indexHost, index, esBatchWriteRetryCount, esBatchWriteRetryWait, esBatchSizeEntries, esNodesWanOnly);
 
-		esCfg.put("es.index.auto.create", "false");
-		esCfg.put("es.nodes", indexHost);
-		esCfg.put("es.mapping.id", "notificationId"); // THE PRIMARY KEY
-		esCfg.put("es.batch.write.retry.count", esBatchWriteRetryCount);
-		esCfg.put("es.batch.write.retry.wait", esBatchWriteRetryWait);
-		esCfg.put("es.batch.size.entries", esBatchSizeEntries);
-		esCfg.put("es.nodes.wan.only", esNodesWanOnly);
-
-		log.info("*** Start indexing");
-		JavaEsSpark.saveJsonToEs(toIndexRdd, index, esCfg);
-		log.info("*** End indexing");
+		indexer.performIndex(dataset, "notificationId");
 
 		log.info("*** Deleting old notifications");
 		final String message = deleteOldNotifications(brokerApiBaseUrl, date - 1000);
@@ -125,12 +102,6 @@ public class IndexNotificationsJob {
 				return IOUtils.toString(response.getEntity().getContent());
 			}
 		}
-	}
-
-	private static String prepareForIndexing(final Notification n, final LongAccumulator acc)
-		throws JsonProcessingException {
-		acc.add(1);
-		return new ObjectMapper().writeValueAsString(n);
 	}
 
 }
