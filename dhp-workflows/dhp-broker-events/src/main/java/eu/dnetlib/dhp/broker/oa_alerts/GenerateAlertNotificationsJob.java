0eeb790297e05cdc0d81e0f066426f1ad89ba003
michele.artini <michele.artini@isti.cnr.it>
2025-06-18 13:54:40 +0200

    partial implementation

--- dhp-workflows/dhp-broker-events/src/main/java/eu/dnetlib/dhp/broker/oa_alerts/GenerateAlertNotificationsJob.java
+++ dhp-workflows/dhp-broker-events/src/main/java/eu/dnetlib/dhp/broker/oa_alerts/GenerateAlertNotificationsJob.java
@@ -1,12 +1,13 @@
+
 package eu.dnetlib.dhp.broker.oa_alerts;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.spark.SparkConf;
@@ -21,12 +22,17 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import eu.dnetlib.broker.objects.alerts.ValidatorAlertMessage;
 import eu.dnetlib.broker.objects.alerts.ValidatorErrorMessage;
 import eu.dnetlib.dhp.application.ArgumentApplicationParser;
+import eu.dnetlib.dhp.broker.model.MappedFields;
 import eu.dnetlib.dhp.broker.model.Notification;
+import eu.dnetlib.dhp.broker.model.Subscription;
 import eu.dnetlib.dhp.broker.oa.util.ClusterUtils;
+import eu.dnetlib.dhp.collection.ApiDescriptor;
 import eu.dnetlib.dhp.common.Constants;
 import eu.dnetlib.dhp.common.SparkSessionSupport;
 import eu.dnetlib.dhp.schema.mdstore.MDStoreVersion;
 import eu.dnetlib.dhp.schema.mdstore.MetadataRecord;
+import eu.dnetlib.dhp.schema.mdstore.Provenance;
+import eu.dnetlib.dhp.schema.mdstore.ValidationType;
 import eu.dnetlib.dhp.utils.DHPUtils;
 import eu.dnetlib.validator2.result_models.StandardValidationResult;
 
@@ -53,64 +59,128 @@ public class GenerateAlertNotificationsJob {
 
 		final String outputPath = parser.get("outputDir");
 
+		final String dsId = DHPUtils.MAPPER.readValue(parser.get("provenance"), Provenance.class).getDatasourceId();
+		log.info("dsId: {}", dsId);
+
+		final String dsName = DHPUtils.MAPPER.readValue(parser.get("provenance"), Provenance.class).getDatasourceName();
+		log.info("dsName: {}", dsName);
+
+		final String compatibilityLevel = DHPUtils.MAPPER.readValue(parser.get("apidescriptor"), ApiDescriptor.class).getCompatibilityLevel();
+		log.info("compatibilityLevel: {}", compatibilityLevel);
+
 		final MDStoreVersion mdstoreVersion = DHPUtils.MAPPER.readValue(mdstoreInputVersion, MDStoreVersion.class);
 		final String inputPath = mdstoreVersion.getHdfsPath() + Constants.MDSTORE_DATA_PATH;
-
 		log.info("inputPath: {}", inputPath);
 
-		final Long date = new Date().getTime();
+		if (StringUtils.isAnyBlank(dsId, compatibilityLevel, inputPath)) { throw new RuntimeException("A required information is missing"); }
+
+		ValidationType validationType;
+		switch (compatibilityLevel) {
+		case "openaire2.0":
+			validationType = ValidationType.openaire2_0;
+			break;
+		case "openaire3.0":
+			validationType = ValidationType.openaire3_0;
+			break;
+		case "openaire4.0":
+			validationType = ValidationType.openaire4_0;
+			break;
+		case "fair_data":
+			validationType = ValidationType.fair_data;
+			break;
+		case "fair_literature_v4":
+			validationType = ValidationType.fair_literature_v4;
+			break;
+		default:
+			validationType = null;
+			break;
+		}
+
+		if (validationType == null) {
+			log.warn("The compatibility is non managed by the validator engine");
+			return;
+		}
+
+		final String topic = "ALERT/" + validationType;
+
+		log.info("topic: {}", topic);
 
 		final SparkConf conf = new SparkConf();
 
 		SparkSessionSupport.runWithSparkSession(conf, isSparkSessionManaged, spark -> {
 			final LongAccumulator total = spark.sparkContext().longAccumulator("total_alert_notifications");
 
-			// TODO: Calcolare il totale per ogni topic e aggiornare il database del broker usando l'api
-			// TODO: Incrociare le potenziali notifiche con le subscription
-			// TODO: Indicizzare
-
-			final Dataset<Notification> dataset = spark.read()
+			final Dataset<ValidatorAlertMessage> payloads = spark
+					.read()
 					.parquet(inputPath)
 					.as(Encoders.bean(MetadataRecord.class))
-					.flatMap(r -> generateAlertNotifications(r, date), Encoders.bean(Notification.class));
+					.filter(r -> r.getValidationResults().containsKey(validationType))
+					.map(r -> generatePayload(r.getOriginalId(), dsId, dsName, r.getValidationResults().get(validationType)), Encoders
+							.bean(ValidatorAlertMessage.class));
 
-			ClusterUtils.save(dataset, outputPath, Notification.class, total);
+			updateStats(dsId, topic, payloads.count());
+
+			final List<Subscription> subscriptions = listSubscriptions(dsId, topic);
+
+			final Long date = new Date().getTime();
+			log.info("date: {}", date);
+
+			if (subscriptions.size() > 0) {
+
+				final Dataset<Notification> dataset =
+						payloads.flatMap(p -> generateAlertNotifications(p, date, subscriptions).iterator(), Encoders.bean(Notification.class))
+								.filter(n -> StringUtils.isNotBlank(n.getPayload()));
+
+				ClusterUtils.save(dataset, outputPath, Notification.class, total);
+			} else {
+				ClusterUtils.save(spark.emptyDataset(Encoders.bean(Notification.class)), outputPath, Notification.class, total);
+			}
 		});
 	}
 
-	private static Iterator<Notification> generateAlertNotifications(final MetadataRecord r, final Long date) {
-		if ((r == null)
-				|| (r.getProvenance() == null)
-				|| StringUtils.isAnyBlank(r.getOriginalId(), r.getProvenance().getDatasourceId(), r.getProvenance().getDatasourceName())) {
-			return new ArrayList<Notification>().iterator();
-		}
+	private static List<Notification> generateAlertNotifications(final ValidatorAlertMessage alertMessage,
+			final Long date,
+			final List<Subscription> subscriptions) {
 
-		final String originalId = r.getOriginalId();
-		final String dsId = r.getProvenance().getDatasourceId();
-		final String dsName = r.getProvenance().getDatasourceName();
+		final MappedFields fields = new MappedFields();
 
-		return r.getValidationResults()
-				.entrySet()
-				.stream()
-				.map(e -> {
-					// TODO
-					final Notification n = new Notification();
-					n.setNotificationId(null);
-					n.setEventId(null);
-					n.setDate(date);
-					n.setMap(null);
-					n.setPayload(generatePayload(originalId, dsId, dsName, e.getValue()));
-					n.setProducerId(null);
-					n.setSubscriptionId(null);
-					n.setTopic(null);
-					return n;
-				})
-				.filter(n -> StringUtils.isNotBlank(n.getPayload()))
-				.iterator();
+		// TODO correggere il modello
+		// fields.put("originalId", alertMessage.getOriginalId());
+		// fields.put("datasourceId", alertMessage.getDatasourceId());
+		// fields.put("datasourceName", alertMessage.getDatasourceName());
+
+		final String eventId = "evt-" + UUID.randomUUID();
+
+		return subscriptions.stream().map(s -> {
+			final Notification n = new Notification();
+			n.setNotificationId("ntf-" + DigestUtils.md5Hex(s.getSubscriptionId() + "@@@" + eventId));
+			n.setEventId(eventId);
+			n.setDate(date);
+			n.setMap(fields);
+
+			try {
+				n.setPayload(DHPUtils.MAPPER.writeValueAsString(alertMessage));
+			} catch (final JsonProcessingException e) {
+				n.setPayload(null);
+			}
+			n.setProducerId("OPENAIRE");
+			n.setSubscriptionId(s.getSubscriptionId());
+			n.setTopic(s.getTopic());
+			return n;
+		}).collect(Collectors.toList());
+	}
+
+	private static List<Subscription> listSubscriptions(final String dsId, final String topic) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static void updateStats(final String dsId, final String topic, final long count) {
+		// TODO Auto-generated method stub
 
 	}
 
-	private static String generatePayload(final String originalId,
+	private static ValidatorAlertMessage generatePayload(final String originalId,
 			final String dsId,
 			final String dsName,
 			final StandardValidationResult standardValidationResult) {
@@ -119,12 +189,14 @@ public class GenerateAlertNotificationsJob {
 		res.setOriginalId(originalId);
 		res.setDatasourceId(dsId);
 		res.setDatasourceName(dsName);
-		res.setErrors(standardValidationResult.getResults()
+		res.setErrors(standardValidationResult
+				.getResults()
 				.entrySet()
 				.stream()
 				.map(e -> {
 					final String field = e.getKey();
-					return e.getValue()
+					return e
+							.getValue()
 							.getErrors()
 							.stream()
 							.map(err -> new ValidatorErrorMessage(field, err))
@@ -133,12 +205,7 @@ public class GenerateAlertNotificationsJob {
 				.flatMap(List::stream)
 				.collect(Collectors.toList()));
 
-		try {
-			return DHPUtils.MAPPER.writeValueAsString(standardValidationResult);
-		} catch (final JsonProcessingException e1) {
-			log.warn("Error serializing payload");
-			return null;
-		}
+		return res;
 
 	}
 
