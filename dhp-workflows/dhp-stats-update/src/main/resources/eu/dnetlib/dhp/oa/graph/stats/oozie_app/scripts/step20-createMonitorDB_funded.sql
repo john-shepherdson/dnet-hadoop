c882a93b1c819aff991626af5c66bf0d905032df
Claudio Atzori <claudio.atzori@isti.cnr.it>
2025-07-09 09:00:17 +0200

    stats related modules moved in dhp-graph-stats project

--- dhp-workflows/dhp-stats-update/src/main/resources/eu/dnetlib/dhp/oa/graph/stats/oozie_app/scripts/step20-createMonitorDB_funded.sql
+++
@@ -1,9 +0,0 @@
-set mapred.job.queue.name=analytics;
-
-drop database if exists TARGET cascade;
-create database if not exists TARGET;
-
-create table TARGET.result stored as parquet as
-    select distinct * from (
-        select * from SOURCE.result r where exists (select 1 from SOURCE.result_projects rp join SOURCE.project p on rp.project=p.id where rp.id=r.id)
-    ) foo;
